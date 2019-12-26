8555139aa4a70cf5f630dc29c69a8681019bf72c
Lea Vauchier <lea.vauchier@kitware.com>
2019-12-26 14:59:34 +0100

    Add SegmentedCloudTransformations module

--- LVCore/LidarPlugin/StandAloneTools/PostProcessLabelCloud.cxx
+++ LVCore/LidarPlugin/StandAloneTools/PostProcessLabelCloud.cxx
@@ -68,19 +68,16 @@ It successively:
 #include "vtkPCLConversions.h"
 #include "vtkTemporalTransforms.h"
 #include "vtkCustomTransformInterpolator.h"
-#include "CameraModel.h"
 #include "vtkBoundingBoxReader.h"
-#include "vtkBoundingBox.h"
 #include "CameraProjection.h"
 #include "BoundingBox.h"
 #include "vtkEigenTools.h"
 #include "CategoriesConfig.h"
 #include "FramesSeriesUtils.h"
-#include "KDTreeVectorOfVectorsAdaptor.h"
+#include "SegmentedCloudTransformations.h"
 
 // STD
 #include <iostream>
-#include <math.h>
 
 #include <cassert>
 
@@ -104,170 +101,8 @@ It successively:
 #include <vtkMultiBlockDataSet.h>
 #include <vtkFieldData.h>
 #include <vtk_jsoncpp.h>
-#include <vtkHelper.h>
 #include <vtkDoubleArray.h>
 #include <vtkMath.h>
-
-// Helper classes -------------------------------------------------------------
-class Segment
-{
-public:
-  bool ignore;
-  bool isThing;
-  int segmentId;
-  int categoryId;
-  std::vector<unsigned int> pointIndices;
-  float confidence;
-
-  Segment() : ignore(true), isThing(false), segmentId(-1), categoryId(0), confidence(-1) {}
-
-  Segment(YAML::Node segment, CategoriesConfig*  catConfig)
-  {
-    int categoryId = segment["category_id"].as<int>();
-    int segmentId = segment["id"].as<int>();
-    this->ignore = catConfig->IsCategoryIgnored(categoryId);
-    this->isThing = catConfig->IsCategoryThing(categoryId);
-    this->segmentId = segmentId;
-    this->categoryId = categoryId;
-    if (this->isThing)
-    {
-      this->confidence = segment["score"].as<float>();
-    }
-  }
-};
-
-// Outliers filtering functions ----------------------------------------------
-
-template<typename T>
-T Median(const std::vector<T> in_vect)
-{
-  std::vector<T> out_vect = in_vect;
-  sort(out_vect.begin(), out_vect.end());
-
-  if (out_vect.size() % 2 == 0)
-  {
-    return (out_vect[out_vect.size() / 2] + out_vect[out_vect.size() / 2 + 1])/ 2;
-  }
-  else
-  {
-    return out_vect[out_vect.size() / 2] ;
-  }
-}
-
-// Inspired from https://medium.com/james-blogs/outliers-make-us-go-mad-univariate-outlier-detection-b3a72f1ea8c7
-std::vector<bool> GetOutliersInCloudSubset(vtkSmartPointer<vtkPolyData> cloud,
-                                           std::vector<unsigned int> subsetPointIndices,
-                                           double cutoff=3.5)
-{
-  size_t nbPoints = subsetPointIndices.size();
-  if (nbPoints == 0)
-  {
-    return {};
-  }
-
-  // Compute position of median point
-  std::vector<double> Xs(nbPoints);
-  std::vector<double> Ys(nbPoints);
-  std::vector<double> Zs(nbPoints);
-
-  // TODO : check if median of independant coordinates matches median point
-  // maybe https://en.wikipedia.org/wiki/Geometric_median is better
-  for (size_t i = 0; i < nbPoints; ++i)
-  {
-    double pt[3];
-    cloud->GetPoint(subsetPointIndices[i], pt);
-    Xs[i] = pt[0];
-    Ys[i] = pt[1];
-    Zs[i] = pt[2];
-  }
-  Eigen::Vector3d medPt(Median(Xs), Median(Ys), Median(Zs));
-
-  // Compute Median distance to medPt
-  std::vector<double> distToMedPt(nbPoints);
-  for (size_t i = 0; i < nbPoints; ++i)
-  {
-    double pt[3];
-    cloud->GetPoint(subsetPointIndices[i], pt);
-    Eigen::Vector3d X(pt[0] - medPt[0], pt[1] - medPt[1], pt[2] - medPt[2]);
-    distToMedPt[i] = X.norm();
-  }
-  // TODO: check case mad = 0
-  double mad = Median(distToMedPt);
-
-  // Modified Z-score
-  std::vector<double> modifiedZScore(nbPoints);
-  for (size_t i = 0; i < nbPoints; ++i)
-  {
-    double pt[3];
-    cloud->GetPoint(subsetPointIndices[i], pt);
-    // 0.6745 is the 0.75th quartile of the standard normal distribution, to which the MAD converges to.
-    modifiedZScore[i] = (0.6745 * distToMedPt[i]) / mad;
-  }
-
-  // Determine outliers
-  std::vector<bool> isOutLier(nbPoints, true);
-  for (size_t i = 0; i < nbPoints; ++i)
-  {
-    if (modifiedZScore[i] <= cutoff)
-    {
-      isOutLier[i] = false;
-    }
-  }
-
-  return isOutLier;
-}
-
-void RemoveOutliers(Segment* segment, vtkSmartPointer<vtkPolyData> cloud)
-{
-  std::vector<unsigned int> remainPointIndices(0);
-  std::vector<bool> isOutlier = GetOutliersInCloudSubset(cloud, segment->pointIndices);
-  for (size_t i = 0; i < segment->pointIndices.size(); ++i)
-  {
-    if (!isOutlier[i])
-    {
-      remainPointIndices.push_back(segment->pointIndices[i]);
-    }
-  }
-  segment->pointIndices = remainPointIndices;
-}
-
-
-void RemovePointsCloserThanThreshold(Segment* segment,
-                                     vtkSmartPointer<vtkPolyData> cloud,
-                                     float threshold = 0)
-{
-  auto distanceArray = cloud->GetPointData()->GetArray("distance_m");
-  std::vector<unsigned int> remainPointIndices(0);
-  for (size_t i = 0; i < segment->pointIndices.size(); ++i)
-  {
-    if (distanceArray->GetTuple1(segment->pointIndices[i]) >= threshold)
-    {
-      remainPointIndices.push_back(segment->pointIndices[i]);
-    }
-  }
-  segment->pointIndices = remainPointIndices;
-}
-
-
-std::vector<Segment> GetSegmentsWithMorePtsThanThreshold(std::vector<Segment> segmentsList,
-                                                         unsigned int threshold = 0)
-{
-  std::vector<unsigned int> remainSegmentIndices(0);
-  for (size_t i = 0; i < segmentsList.size(); ++i)
-  {
-    if (segmentsList[i].pointIndices.size() >= threshold)
-    {
-      remainSegmentIndices.push_back(i);
-    }
-  }
-  std::vector<Segment> outSegmentsList;
-  for (auto i: remainSegmentIndices)
-  {
-    outSegmentsList.push_back(segmentsList[i]);
-  }
-
-  return outSegmentsList;
-}
 
 
 // Bbox utils -----------------------------------------------------------------
@@ -292,47 +127,6 @@ Eigen::Matrix3d MaybeGetRotationMatrixFromInterpolator(vtkSmartPointer<vtkCustom
     std::cout << "Consider increasing timespan" << std::endl;
   }
   return R;
-}
-
-
-OrientedBoundingBox<3> CreateBboxFromSegment(Segment* segment,
-                             vtkSmartPointer<vtkPolyData> cloud,
-                             Eigen::Matrix3d BboxesRotation=Eigen::Matrix3d::Identity())
-{
-  vtkBoundingBox vtkBbox;
-  vtkDataArray* timeArray = cloud->GetPointData()->GetArray("adjustedtime");
-  // TODO: find a better time than the one of the first point
-  int time = timeArray->GetTuple1(segment->pointIndices[0]);
-
-  // vtkBbox aligned to interpolator axes (rotation only)
-  for (size_t i = 0; i < segment->pointIndices.size(); ++i)
-  {
-    double pt[3];
-    cloud->GetPoint(segment->pointIndices[i], pt);
-    Eigen::Vector3d X(pt[0], pt[1], pt[2]);
-    Eigen::Vector3d X1 = BboxesRotation.transpose() * X;
-    vtkBbox.AddPoint(X1[0],X1[1], X1[2]);
-  }
-
-  double center[3];
-  vtkBbox.GetCenter(center);
-  Eigen::Vector3d C(center[0], center[1], center[2]);
-
-  double bounds[6]; // double xMin, double xMax, double yMin, double yMax, double zMin, double zMax
-  vtkBbox.GetBounds(bounds);
-
-  Eigen::Vector3d D(bounds[1] - bounds[0], bounds[3] - bounds[2], bounds[5] - bounds[4]);
-
-  // Create bbox in original coordinates
-  OrientedBoundingBox<3> bbox;
-  bbox.Center = BboxesRotation * C;
-  bbox.Width =  D;
-  bbox.Orientation = BboxesRotation;
-  bbox.ClassId = segment->categoryId;
-  bbox.Confidence = static_cast<int>(segment->confidence * 100);
-  bbox.TimeStamp = time;
-
-  return bbox;
 }
 
 
@@ -383,113 +177,6 @@ void Export3DBBAsYaml(std::vector<OrientedBoundingBox<3>> objects,
   fout.close();
 }
 
-// -----------------------------------------------------------------------------
-
-// https://stackoverflow.com/questions/2488941/find-which-numbers-appears-most-in-a-vector
-template<class InputIt, class T = typename std::iterator_traits<InputIt>::value_type>
-T mostCommon(InputIt begin, InputIt end)
-{
-  std::map<T, int> counts;
-  for (InputIt it = begin; it != end; ++it) {
-    if (counts.find(*it) != counts.end()) {
-      ++counts[*it];
-    }
-    else {
-      counts[*it] = 1;
-    }
-  }
-  return std::max_element(counts.begin(), counts.end(),
-    [] (const std::pair<T, int>& pair1, const std::pair<T, int>& pair2) {
-    return pair1.second < pair2.second;})->first;
-}
-
-
-std::vector<Segment> ApplyKNNsToSegments(vtkSmartPointer<vtkPolyData> cloud,
-                                         std::vector<Segment> segmentsList,
-                                         size_t nbNeighbors,
-                                         double maxDistance=std::numeric_limits<double>::max()
-                                         )
-{
-  // Create Segment list with empty indices
-  std::vector<Segment> outSegmentsList = segmentsList;
-  std::map<int, size_t> segmentIdMap;
-  for (size_t i = 0; i < outSegmentsList.size(); ++i)
-  {
-    outSegmentsList[i].pointIndices = {};
-    segmentIdMap.insert(std::make_pair(outSegmentsList[i].segmentId, i));
-  }
-
-  // Prepare KD-tree
-  std::vector<std::vector<double>> points;
-  std::vector<int> idxArray;
-  for (unsigned int pointIdx = 0; pointIdx < cloud->GetNumberOfPoints(); ++pointIdx)
-  {
-    double* p = cloud->GetPoint(pointIdx);
-    std::vector<double> apoint = {p[0], p[1], p[2]};
-    points.push_back(apoint);
-    idxArray.push_back(pointIdx);
-  }
-  typedef std::vector<std::vector<double> > my_vector_of_vectors_t;
-  typedef KDTreeVectorOfVectorsAdaptor< my_vector_of_vectors_t, double > my_kd_tree_t;
-
-  my_kd_tree_t mat_index(-1 /*dim*/, points, 10 /* max leaf */ );
-  mat_index.index->buildIndex();
-
-  vtkDataArray* segmentArray = cloud->GetPointData()->GetArray("segment");
-
-  // Use apply nearest neighbors segment value
-
-  for (unsigned int pointIdx = 0; pointIdx < cloud->GetNumberOfPoints(); ++pointIdx)
-  {
-    // TODO: Add distance weights for nearest neighbors values
-    std::vector<size_t> nearestIndex(nbNeighbors, -1);
-    std::vector<double> nearestDist(nbNeighbors, -1.0);
-    std::vector<int> nearestSegments(0);
-
-    mat_index.query(points[pointIdx].data(), nbNeighbors, nearestIndex.data(), nearestDist.data());
-
-    // get segment value for neighbors
-    for (size_t i = 0; i < nbNeighbors; ++i)
-    {
-      if ((nearestDist[i] >= 0) && (nearestDist[i] <= maxDistance)) {
-        nearestSegments.push_back(segmentArray->GetTuple1(nearestIndex[i]));
-      }
-    }
-    if (nearestSegments.size() > 0)
-    {
-      int mostCommonSegment = mostCommon(nearestSegments.begin(), nearestSegments.end());
-
-      // Add point to corresponding segment
-      if (segmentIdMap.find(mostCommonSegment) != segmentIdMap.end())
-      {
-        outSegmentsList[segmentIdMap[mostCommonSegment]].pointIndices.push_back(pointIdx);
-      }
-    }
-  }
-  return outSegmentsList;
-}
-
-// -----------------------------------------------------------------------------
-
-
-void OverwriteCloudSegmentInfos(std::vector<Segment> segmentsList,
-                                vtkSmartPointer<vtkPolyData> outCloud)
-{
-  outCloud->GetPointData()->RemoveArray("segment");
-  outCloud->GetPointData()->RemoveArray("category");
-
-  vtkSmartPointer<vtkIntArray> segmentArray = addArrayWithDefault<vtkIntArray, int>("segment", outCloud, 0);
-  vtkSmartPointer<vtkIntArray> categoryArray = addArrayWithDefault<vtkIntArray, int>("category", outCloud, 0);
-
-  for (size_t segmentIdx = 0; segmentIdx < segmentsList.size(); ++segmentIdx)
-  {
-    for (auto pointIdx : segmentsList[segmentIdx].pointIndices)
-    {
-      segmentArray->SetTuple1(pointIdx, segmentsList[segmentIdx].segmentId);
-      categoryArray->SetTuple1(pointIdx, segmentsList[segmentIdx].categoryId);
-    }
-  }
-}
 
 // ----------------------------------------------------------------------------
 
@@ -497,61 +184,25 @@ void RefineLabelCloudWithInstances(vtkSmartPointer<vtkPolyData> cloud,
                                    YAML::Node yamlSegments,
                                    vtkSmartPointer<vtkPolyData> outCloud,  // Will store refined Cloud
                                    std::vector<OrientedBoundingBox<3>>* objects,   // Will store instances 3D Bboxes
-                                   CategoriesConfig*  catConfig,
+                                   CategoriesConfig* catConfig,
                                    Eigen::Matrix3d BboxesRotation=Eigen::Matrix3d::Identity())
 {
-  // Initialise segments
-  std::vector<Segment> segmentsList;
-  for (size_t segmentIdx = 0; segmentIdx < yamlSegments.size(); ++segmentIdx)
-  {
-    segmentsList.push_back(Segment(yamlSegments[segmentIdx], catConfig));
-  }
+  SegmentedCloud segmentedCloud(cloud, yamlSegments, catConfig);
 
-  // Populate segments
-  vtkDataArray* segmentArray = cloud->GetPointData()->GetArray("segment");
-  for (unsigned int pointIdx = 0; pointIdx < cloud->GetNumberOfPoints(); ++pointIdx)
-  {
-    int ptSegmentId = segmentArray->GetTuple1(pointIdx);
-    for (size_t i = 0; i < segmentsList.size(); ++i)
-    {
-      if (ptSegmentId == segmentsList[i].segmentId)
-      {
-        segmentsList[i].pointIndices.push_back(pointIdx);
-      }
-    }
-  }
-
-  // Refine segments
   int nbNeighbors = 10;
   double maxDistance = 0.3; // in meters
-  segmentsList = ApplyKNNsToSegments(cloud, segmentsList, nbNeighbors, maxDistance);
+  segmentedCloud.ApplyKNNsToSegments(nbNeighbors, maxDistance);
+  segmentedCloud.ApplyDistanceThreshold(1.3);
+  segmentedCloud.RemoveSegmentsOutliers();
+  segmentedCloud.RemoveSegmentsWithLessPointsThanThreshold(5);
+  auto bboxes = segmentedCloud.CreateBoundingBoxes(BboxesRotation);
+  segmentedCloud.UpdateCloud();
+  outCloud->DeepCopy(segmentedCloud.GetPointCloud());
 
-  for (size_t segmentIdx = 0; segmentIdx < segmentsList.size(); ++segmentIdx)
+  for (auto bb: bboxes)
   {
-    RemovePointsCloserThanThreshold(&(segmentsList[segmentIdx]), cloud, 1.3);
+    objects->push_back(bb);
   }
-
-  for (size_t segmentIdx = 0; segmentIdx < segmentsList.size(); ++segmentIdx)
-  {
-    RemoveOutliers(&(segmentsList[segmentIdx]), cloud);
-  }
-
-  segmentsList = GetSegmentsWithMorePtsThanThreshold(segmentsList, 5);
-
-  // Create bboxes for "thing" segments only
-  for (size_t segmentIdx = 0; segmentIdx < segmentsList.size(); ++segmentIdx)
-  {
-    if (segmentsList[segmentIdx].isThing
-        && !segmentsList[segmentIdx].ignore
-        && segmentsList[segmentIdx].pointIndices.size() > 0)
-    {
-      objects->push_back(CreateBboxFromSegment(&segmentsList[segmentIdx], cloud));
-    }
-  }
-
-  // Copy labelcloud excluding labels
-  outCloud->DeepCopy(cloud);
-  OverwriteCloudSegmentInfos(segmentsList, outCloud);
 }
 
 
@@ -670,7 +321,6 @@ int main(int argc, char* argv[])
       std::cout << "/" <<(lastLidarFrameToProcess - firstLidarFrameToProcess);
       std::cout << std::endl;
     }
-
   }
   outputCloudSeries.WriteToFile();
   outputBboxSeries.WriteToFile();
