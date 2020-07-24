2a3dddee6a55318a5a1c5202d786c2b03030bfa4
Melanie Carriere <melanie.carriere@kitware.com>
2020-07-24 16:26:54 +0200

    Move the structures declaration's of NetworkPacket to the header file
    the new StandAloneTools "PCAPSeparator" need them.

--- LVCore/LidarPlugin/Common/Network/NetworkPacket.cxx
+++ LVCore/LidarPlugin/Common/Network/NetworkPacket.cxx
@@ -2,7 +2,6 @@
 #include <cstring>
 #include <iostream>
 
-#include <boost/endian/arithmetic.hpp>
 #ifdef _MSC_VER
 #include <windows.h>
 
@@ -23,51 +22,6 @@ int gettimeofday(struct timeval* tp, void*)
 }
 
 #endif
-
-#pragma pack(push, 1)
-struct ethernet_header
-{
-  boost::endian::big_uint48_t destination_mac_address {0xffffffffffff};
-  boost::endian::big_uint48_t source_mac_address {0xffffffffffff};
-  boost::endian::big_uint16_t packet_type {0x0800}; // IP
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct ipv4_header
-{
-  boost::endian::big_uint8_t version_and_IHL {0x45};
-  boost::endian::big_uint8_t service {0x00};
-  boost::endian::big_uint16_t total_length {0x04d2};
-  boost::endian::big_uint16_t identification {0x0000};
-  boost::endian::big_uint16_t flag_and_fragment_offset {0x4000}; // do not fragment
-  boost::endian::big_uint8_t time_to_live {0xff};
-  boost::endian::big_uint8_t protocol {0x11}; // UDP
-  boost::endian::big_uint16_t checksum {0xffff};
-  boost::endian::big_uint32_t source_ip_address {0xc0a801c8}; // 192.168.1.200
-  boost::endian::big_uint32_t destination_ip_address {0xffffffff}; // broadcast
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct upd_header
-{
-  boost::endian::big_uint16_t source_port {2368};
-  boost::endian::big_uint16_t destination_port {2368};
-  boost::endian::big_uint16_t length {0x04be};
-  boost::endian::big_uint16_t checksum {0x0000};
-};
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-struct Ethernet_IPV4_UDP_Header
-{
-  ethernet_header ethernet;
-  ipv4_header ipv4;
-  upd_header upd;
-};
-#pragma pack(pop)
-
 
 //------------------------------------------------------------------------------
 NetworkPacket* NetworkPacket::BuildEthernetIP4UDP(const unsigned char* payload,
