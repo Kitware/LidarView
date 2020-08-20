b03026e0f3d374b9a2a950850efb4ce3f87fc6dc
Melanie Carriere <melanie.carriere@kitware.com>
2020-08-20 14:48:42 +0200

    Update the recording of pcap on the new lqStreamRecordReaction

--- Application/Ui/python/lidarview/applogic.py
+++ Application/Ui/python/lidarview/applogic.py
@@ -153,7 +153,6 @@ def openData(filename):
     app.actions['actionSavePCAP'].setEnabled(False)
     app.actions['actionChoose_Calibration_File'].setEnabled(False)
     app.actions['actionCropReturns'].setEnabled(False)
-    app.actions['actionRecord'].setEnabled(False)
     app.actions['actionShowRPM'].enabled = True
 
 
@@ -401,8 +400,6 @@ def openSensor():
     app.actions['actionMeasurement_Grid'].setChecked(True)
     showMeasurementGrid()
 
-    app.actions['actionRecord'].enabled = True
-
     updateUIwithNewLidar()
 
 
@@ -529,7 +526,6 @@ def openPCAP(filename, positionFilename=None, calibrationFilename=None, calibrat
 
     enableSaveActions()
     addRecentFile(filename)
-    app.actions['actionRecord'].setEnabled(False)
 
     app.actions['actionShowRPM'].enabled = True
 
@@ -1068,7 +1064,6 @@ def close():
     app.filenameLabel.setText('')
     app.statusLabel.setText('')
     disableSaveActions()
-    app.actions['actionRecord'].setChecked(False)
 
 
 def _setSaveActionsEnabled(enabled):
@@ -1087,44 +1082,6 @@ def enableSaveActions():
 def disableSaveActions():
     _setSaveActionsEnabled(False)
     app.actions['actionSavePositionCSV'].setEnabled(False)
-
-
-def recordFile(filename):
-
-    sensor = getSensor()
-    if sensor:
-        stopStream()
-        sensor.OutputFile = filename
-        app.statusLabel.setText('  Recording file: %s.' % os.path.basename(filename))
-        startStream()
-
-
-def onRecord():
-
-    recordAction = app.actions['actionRecord']
-
-    if not recordAction.isChecked():
-        stopRecording()
-
-    else:
-
-        fileName = getSaveFileName('Choose Output File', 'pcap', getDefaultSaveFileName('pcap'))
-        if not fileName:
-            recordAction.setChecked(False)
-            return
-
-        recordFile(fileName)
-    recordAction.setChecked(recordAction.isChecked())
-
-
-def stopRecording():
-
-    app.statusLabel.setText('')
-    sensor = getSensor()
-    if sensor:
-        stopStream()
-        sensor.OutputFile = ''
-        startStream()
 
 
 def startStream():
@@ -1617,18 +1574,11 @@ def setupActions():
     for a in actions:
         app.actions[a.objectName] = a
 
-    app.actions['actionRecord'] = QtGui.QAction( \
-      QtGui.QIcon(QtGui.QPixmap(':/LidarViewPlugin/media-record.png')), \
-      "actionRecord",\
-      mW)
-    app.actions['actionRecord'].setCheckable(True)
-
     app.actions['actionAdvanceFeature'].connect('triggered()', onToogleAdvancedGUI)
 
     app.actions['actionPlaneFit'].connect('triggered()', planeFit)
 
     app.actions['actionClose'].connect('triggered()', close)
-    app.actions['actionRecord'].connect('triggered()', onRecord)
     app.actions['actionSaveCSV'].connect('triggered()', onSaveCSV)
     app.actions['actionSavePositionCSV'].connect('triggered()', onSavePosition)
     app.actions['actionSaveLAS'].connect('triggered()', onSaveLAS)
@@ -1687,7 +1637,9 @@ def setupActions():
     # Setup and add the playback speed control toolbar
     timeToolBar = mW.findChild('QToolBar','Player Control')
 
+    # Place the record button at the right place
     timeToolBar.addAction(app.actions['actionRecord'])
+
     spinBoxLabel = QtGui.QLabel('TF:')
     spinBoxLabel.toolTip = "Number of trailing frames"
     timeToolBar.addWidget(spinBoxLabel)
