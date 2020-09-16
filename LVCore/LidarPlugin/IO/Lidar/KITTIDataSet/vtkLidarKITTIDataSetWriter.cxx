e07618a90233c25c60dcb2a6e472db736c2af1c8
Lea Vauchier <lea.vauchier@kitware.com>
2020-09-16 10:42:58 +0200

    Add LidarKITTIDataSetWriter writer as a filter
    This is a temporary implementation, this writer should be implemented as
    a writer instead of a filter.

---
+++ LVCore/LidarPlugin/IO/Lidar/KITTIDataSet/vtkLidarKITTIDataSetWriter.cxx
@@ -0,0 +1,148 @@
+#include "vtkLidarKITTIDataSetWriter.h"
+
+#include <vtkInformationVector.h>
+#include <vtkInformation.h>
+#include <vtkPointData.h>
+#include <vtkStreamingDemandDrivenPipeline.h>
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+#include <boost/filesystem.hpp>
+
+namespace
+{
+/*
+ * @brief ParseCloudData parses a point cloud vtkPolyData to retrieve information
+ * to export as a vector of floats containing (x, y, z, intensity) for each
+ * point.
+ * */
+std::vector<float> ParseCloudData(vtkSmartPointer<vtkPolyData> cloud)
+{
+  std::vector<float> dataToWrite(4 * cloud->GetNumberOfPoints());
+
+  // TODO: select intensity array
+  vtkDataArray* intensity = cloud->GetPointData()->GetArray("intensity");
+  for (int pointIndex = 0; pointIndex < cloud->GetNumberOfPoints(); ++pointIndex)
+  {
+    double* pos = cloud->GetPoint(pointIndex);
+    dataToWrite[4 * pointIndex] = static_cast<float>(pos[0]);
+    dataToWrite[4 * pointIndex + 1] = static_cast<float>(pos[1]);
+    dataToWrite[4 * pointIndex + 2] = static_cast<float>(pos[2]);
+    dataToWrite[4 * pointIndex + 3] = static_cast<float>(intensity->GetTuple1(pointIndex));
+  }
+
+  return dataToWrite;
+}
+
+
+/*
+ * @brief WriteToBinaryFile writes the information to export in a binary file
+ * */
+void WriteToBinaryFile(std::vector<float> &inData, std::string filename)
+{
+  ofstream fout(filename, ios::out | ios::binary);
+  if (!fout.is_open())
+  {
+    std::cout << "Error: could not open file " << filename << " for writing." << std::endl;
+  }
+  else
+  {
+    fout.write(reinterpret_cast<char*>(&inData[0]), inData.size() * sizeof(float));
+    fout.close();
+  }
+}
+}
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkLidarKITTIDataSetWriter)
+
+//-----------------------------------------------------------------------------
+vtkLidarKITTIDataSetWriter::vtkLidarKITTIDataSetWriter()
+{
+// TODO: set it as a writer
+//  this->SetNumberOfOutputPorts(0);
+}
+
+
+void vtkLidarKITTIDataSetWriter::UpdatePipelineIndex(vtkInformation * inInfo)
+{
+  // Save current pipeline time step
+  this->PipelineTime = inInfo->Get(vtkStreamingDemandDrivenPipeline::UPDATE_TIME_STEP());
+
+  // TODO check best way to get the pipeline index
+
+  double *time_steps = inInfo->Get(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+  int nb_time_steps = inInfo->Length(vtkStreamingDemandDrivenPipeline::TIME_STEPS());
+  std::vector<double> TimeSteps(time_steps, time_steps + nb_time_steps);
+  // get the index corresponding to the requested pipeline time
+  // find the index of the first time step that is not less than pipeline time
+  this->PipelineIndex = std::distance(TimeSteps.begin(),
+                                      std::lower_bound(TimeSteps.begin(),
+                                                       TimeSteps.end(),
+                                                       this->PipelineTime));
+
+  // check if the previous index is closer
+  if (this->PipelineIndex > 0)
+  {
+ if (TimeSteps[this->PipelineIndex] - this->PipelineTime > this->PipelineTime - TimeSteps[this->PipelineIndex - 1])
+    {
+      this->PipelineIndex -= 1;
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+int vtkLidarKITTIDataSetWriter::RequestUpdateExtent(vtkInformation* vtkNotUsed(request),
+                                      vtkInformationVector** inputVector,
+                                      vtkInformationVector* vtkNotUsed(outputVector))
+{
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  this->UpdatePipelineIndex(inInfo);
+
+  return 1;
+}
+
+//-----------------------------------------------------------------------------
+void vtkLidarKITTIDataSetWriter::SetFolderName(std::string foldername)
+{
+  if (foldername.empty())
+  {
+    this->FolderName = "";
+    return;
+  }
+  if (!boost::filesystem::is_directory(foldername))
+  {
+     boost::filesystem::create_directory(foldername);
+  }
+  this->FolderName = foldername;
+  return;
+}
+
+//-----------------------------------------------------------------------------
+int vtkLidarKITTIDataSetWriter::RequestData(vtkInformation *, vtkInformationVector **inputVector, vtkInformationVector *outputVector)
+{
+  if (this->FolderName.empty())
+  {
+    vtkErrorMacro("FolderName is empty. Please provide a folder name to write to.");
+  }
+  // Get IO
+  vtkPolyData* inCloud = vtkPolyData::GetData(inputVector[0], 0);
+  vtkPolyData* outCloud = vtkPolyData::GetData(outputVector, 0);
+
+  // Copy input over (TODO: remove this part when using as a writer)
+  outCloud->ShallowCopy(inCloud);
+
+  std::stringstream ss;
+  ss << std::setw(this->NumberOfFileNameDigits) << std::setfill('0') << this->PipelineIndex;
+
+  std::string frameFileName((boost::filesystem::path(this->FolderName) / (ss.str() + ".bin")).string());
+
+  // Data for the .bin file are stored in a std::vector to make it easier to use
+  std::vector<float> dataToWrite = ParseCloudData(inCloud);
+
+  WriteToBinaryFile(dataToWrite, frameFileName);
+
+  return VTK_OK;
+}
---
+++ LVCore/LidarPlugin/IO/Lidar/KITTIDataSet/vtkLidarKITTIDataSetWriter.h
@@ -0,0 +1,65 @@
+#ifndef VTKLIDARKITTIDATASETWRITER_H
+#define VTKLIDARKITTIDATASETWRITER_H
+
+
+#include <vtkPolyDataAlgorithm.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkNew.h>
+
+#include <string>
+
+/*
+ * @brief vtkLidarKITTIDataSetWriter writes point clouds in KITTI Format
+ * The kitti format is:
+ * a binary file for each frame, containing a Nx4 float matrix (with N the number of points)
+ * the data is stored row-aligned (the first 4 values correspond to the first point)
+ * each point is represented by: x, y, z, reflectance
+ *
+ * @warning In the kitti setup, x is pointing forward, and z upwards, make sure
+ * your data is oriented the same way before using this writer
+ **/
+class VTK_EXPORT vtkLidarKITTIDataSetWriter : public vtkPolyDataAlgorithm
+{
+public:
+  static vtkLidarKITTIDataSetWriter* New();
+  vtkTypeMacro(vtkLidarKITTIDataSetWriter, vtkPolyDataAlgorithm);
+  // void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkGetMacro(FolderName, std::string)
+  void SetFolderName(std::string);
+
+  vtkGetMacro(NumberOfFileNameDigits, unsigned int)
+  vtkSetMacro(NumberOfFileNameDigits, unsigned int)
+
+protected:
+  vtkLidarKITTIDataSetWriter();
+  ~vtkLidarKITTIDataSetWriter() = default;
+
+  /*
+   * @brief UpdatePipelineIndex updates tHe pipeline index when requesting a new frame
+   * (used in RequestUpdateExtent)
+   **/
+  void UpdatePipelineIndex(vtkInformation *);
+
+  int RequestUpdateExtent(vtkInformation *, vtkInformationVector **, vtkInformationVector*);
+
+  int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
+
+  // Name of the folder to write the frames to
+  std::string FolderName;
+
+  // Internal variables for pipeline time and index (used to create a filename for each frame)
+  double PipelineTime;
+  unsigned int PipelineIndex;
+
+  // Desired number of digits in the output file names
+  unsigned int NumberOfFileNameDigits = 6;
+
+private:
+  vtkLidarKITTIDataSetWriter(const vtkLidarKITTIDataSetWriter&) = delete;
+  void operator=(const vtkLidarKITTIDataSetWriter&) = delete;
+};
+
+
+#endif // VTKLIDARKITTIDATASETWRITER_H
