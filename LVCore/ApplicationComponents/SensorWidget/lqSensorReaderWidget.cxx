2e156f3fbc8c1e63de62647007f873d4cafb81c2
MelanieCarriere <melanie.carriere@kitware.com>
2021-04-27 13:52:41 +0200

    [ui] Add Lidar Port and Gps Port information in the Sensor widget ui in reader mode
    LidarPort (and GPSPort) are used in case of multi sensors to specify the lidar data to read in the pcap file

--- LVCore/ApplicationComponents/SensorWidget/lqSensorReaderWidget.cxx
+++ LVCore/ApplicationComponents/SensorWidget/lqSensorReaderWidget.cxx
@@ -11,6 +11,9 @@
 lqSensorReaderWidget::lqSensorReaderWidget(QWidget *parent) :
   lqSensorWidget(parent)
 {
+  // In reading mode, we only read the packet received on the right lidarPort.
+  // This is why we want to expose it in reader mode too
+  this->UI->lidarPort->setVisible(true);
 }
 
 //-----------------------------------------------------------------------------
@@ -25,4 +28,23 @@ void lqSensorReaderWidget::onUpdateUI()
   QString pcapFileName = QString::fromStdString(vtkSMPropertyHelper(lidarPropPcapName).GetAsString());
   QString pcapName = "Pcap Name: " + pcapFileName;
   this->UI->PCAPFile->setText(pcapName);
+
+  // Update UI with Lidar Port information
+  vtkSMProperty * lidarPropPort= lidarProxy->GetProperty("LidarPort");
+  QString lidarPort = "Port: " + QString::fromStdString(std::to_string(vtkSMPropertyHelper(lidarPropPort).GetAsInt()));
+  this->UI->lidarPort->setText(lidarPort);
+
+  // Update UI with Position Orientation information
+  if(this->PositionOrientationSource)
+  {
+    this->UI->posOrPort->setVisible(true);
+    vtkSMProxy * posOrProxy = this->PositionOrientationSource->getProxy();
+    vtkSMProperty * posOrPropPort= posOrProxy->GetProperty("PositionOrientationPort");
+    QString posOrPort = "Port: " + QString::fromStdString(std::to_string(vtkSMPropertyHelper(posOrPropPort).GetAsInt()));
+    this->UI->posOrPort->setText(posOrPort);
+  }
+  else
+  {
+    this->UI->posOrPort->setVisible(false);
+  }
 }
