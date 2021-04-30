2deecf6d8ef093498d0e66148ead4aa9aa53fa13
Nick Laurenson <nick.laurenson@kitware.com>
2021-04-30 11:14:15 +0200

    [feat] Add lqLiveSourceScalarColoringBehavior
    This is greatly inspired by LidarView/ouster-studio!98
    This commit generalize the behavior to support any live source and multiple
    views. Also Deleting the representation as done previously lead to a segfault
    from time to times, this may be due to the paraview 5.6 update, as the previous
    behavior has been implemented with paraview 5.4 compatibility.

---
+++ LVCore/ApplicationComponents/lqLiveSourceScalarColoringBehavior.cxx
@@ -0,0 +1,138 @@
+/*=========================================================================
+
+   Program: LidarView
+   Module:  lqLiveSourceScalarColoringBehavior.cxx
+
+   Copyright (c) Kitware Inc.
+   All rights reserved.
+  
+   LidarView is a free software; you can redistribute it and/or modify it
+   under the terms of the LidarView license.
+
+   See LICENSE for the full LidarView license.
+   A copy of this license can be obtained by contacting
+   Kitware Inc.
+   28 Corporate Drive
+   Clifton Park, NY 12065
+   USA
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+========================================================================*/
+#include "lqLiveSourceScalarColoringBehavior.h"
+
+#include <vtkDataObject.h>
+#include <vtkNew.h>
+
+#include <vtkSMSourceProxy.h>
+#include <vtkSMViewProxy.h>
+#include <vtkSMPVRepresentationProxy.h>
+#include <vtkSMParaViewPipelineControllerWithRendering.h>
+#include <vtkPVArrayInformation.h>
+#include <vtkPVDataInformation.h>
+#include <vtkPVDataSetAttributesInformation.h>
+#include <vtkPVXMLElement.h>
+
+#include <pqApplicationCore.h>
+#include <pqPipelineSource.h>
+#include <pqServerManagerModel.h>
+#include <pqView.h>
+
+
+//-----------------------------------------------------------------------------
+lqLiveSourceScalarColoringBehavior::lqLiveSourceScalarColoringBehavior(QObject *parent)
+  : Superclass(parent)
+{
+  //Monitor added sources
+  pqServerManagerModel* smmodel = pqApplicationCore::instance()->getServerManagerModel();
+  this->connect(smmodel, SIGNAL(sourceAdded(pqPipelineSource*)), SLOT(onSourceAdded(pqPipelineSource*)));
+}
+
+//-----------------------------------------------------------------------------
+void lqLiveSourceScalarColoringBehavior::onSourceAdded(pqPipelineSource* src)
+{
+  vtkSMProxy* proxy = src->getProxy();
+  if (auto hints = proxy->GetHints())
+  {
+    if (auto liveHints = hints->FindNestedElementByName("LiveSource"))
+    {
+      //Ask live source to report its updates
+      this->connect(src, SIGNAL(dataUpdated(pqPipelineSource*)), SLOT(onDataUpdated(pqPipelineSource*)));
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+void lqLiveSourceScalarColoringBehavior::onDataUpdated(pqPipelineSource* src)
+{
+  // disconnect dataUpdate Signal, if procedure comes to an end.
+  if (this->TrySetScalarColoring(src))
+  {
+    this->disconnect(src, SIGNAL(dataUpdated(pqPipelineSource*)), this,  SLOT(onDataUpdated(pqPipelineSource*)));
+  }
+}
+
+//-----------------------------------------------------------------------------
+bool lqLiveSourceScalarColoringBehavior::TrySetScalarColoring(pqPipelineSource* src)
+{
+    // check if at least one representation from an output port is using scalar coloring
+    bool hasSucceed = false;
+
+    vtkNew<vtkSMParaViewPipelineController> ppc;
+    auto* ppcwr = vtkSMParaViewPipelineControllerWithRendering::SafeDownCast(ppc);
+    if ( !ppcwr )
+    {
+      std::cerr << __FUNCTION__ << " Pipeline Controller has no rendering capabilities" << std::endl;
+      return true; // Error, Do not try again
+    }
+
+    pqServerManagerModel* smmodel = pqApplicationCore::instance()->getServerManagerModel();
+    auto views = smmodel->findItems<pqView*>();
+    vtkSMSourceProxy* proxy = src->getSourceProxy();
+    int nbPort = proxy->GetNumberOfOutputPorts();
+
+    // for each view
+    foreach (pqView* view, views)
+    {
+      auto* viewProxy = view->getViewProxy();
+      // on each port
+      for (int i = 0; i < nbPort; ++i)
+      {
+        auto* pvrp = vtkSMPVRepresentationProxy::SafeDownCast(viewProxy->FindRepresentation(proxy, i));
+        if (pvrp)
+        {
+          // check if ScalarColoring is already properly set
+          if( !pvrp->GetUsingScalarColoring() )
+          {
+            // check if there is a scalar array
+            if (proxy->GetDataInformation(i) &&
+                proxy->GetDataInformation(i)->GetPointDataInformation() &&
+                proxy->GetDataInformation(i)->GetPointDataInformation()->GetAttributeInformation(vtkDataSetAttributes::SCALARS))
+            {
+              char* arrayName = proxy->GetDataInformation(i)->GetPointDataInformation()->GetAttributeInformation(vtkDataSetAttributes::SCALARS)->GetName();
+              pvrp->SetScalarColoring(arrayName, vtkDataObject::AttributeTypes::POINT);
+              // Do not set hasSucceed to true here, as it will force the rescaling on the next DataUpdated.
+              // This enable to handle case where the Live Source produce incomplete data the first time
+              // This is particulary true for the Lidar Stream, where the first frame is almost always incomplete.
+            }
+          }
+          else
+          {
+            pvrp->RescaleTransferFunctionToDataRange(false, false); // Recale transfertFunction only if it's not loocked
+            hasSucceed = true;
+          }
+        }
+      }
+    }
+  return hasSucceed;
+}
---
+++ LVCore/ApplicationComponents/lqLiveSourceScalarColoringBehavior.h
@@ -0,0 +1,79 @@
+/*=========================================================================
+
+   Program: LidarView
+   Module:  lqLiveSourceScalarColoringBehavior.h
+
+   Copyright (c) Kitware Inc.
+   All rights reserved.
+  
+   LidarView is a free software; you can redistribute it and/or modify it
+   under the terms of the LidarView license.
+
+   See LICENSE for the full LidarView license.
+   A copy of this license can be obtained by contacting
+   Kitware Inc.
+   28 Corporate Drive
+   Clifton Park, NY 12065
+   USA
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+========================================================================*/
+#ifndef LQLIVESOURCESCALARCOLORINGBEHAVIOR_H
+#define LQLIVESOURCESCALARCOLORINGBEHAVIOR_H
+
+#include <QObject>
+
+#include "lqapplicationcomponents_export.h"
+
+class pqPipelineSource;
+class vtkSMSourceProxy;
+
+/**
+ * @class lqLiveSourceScalarColoringBehavior
+ * @ingroup Behaviors
+ *
+ * lqLiveSourceScalarColoringBehavior helps to set ScalarColoring,
+ * once a LiveSource has received data with an active scalar for the first time.
+ * Without it the representation don't set color informations properly
+ */
+class LQAPPLICATIONCOMPONENTS_EXPORT lqLiveSourceScalarColoringBehavior : public QObject
+{
+  Q_OBJECT
+  typedef QObject Superclass;
+
+public:
+  lqLiveSourceScalarColoringBehavior(QObject* parent = 0);
+
+protected slots:
+  /**
+   * @brief sourceAdded check if the source is a live source and connect
+   */
+  void onSourceAdded(pqPipelineSource* src);
+
+  virtual void onDataUpdated(pqPipelineSource* src);
+
+protected:
+  /**
+   * @brief Try to set and rescale the scalar coloring for each representation
+   * Return true, if procedure has completed (successfully or not) and false it the procedure should
+   * be tried again later.
+  */
+  virtual bool TrySetScalarColoring(pqPipelineSource* src);
+
+private:
+
+  Q_DISABLE_COPY(lqLiveSourceScalarColoringBehavior)
+};
+
+#endif // LQLIVESOURCESCALARCOLORINGBEHAVIOR_H
