24fe800a83f5840632b2cd1ef040a5b119d6b3a5
Arnaud Billon <arnaud.billon@kitware.com>
2021-06-03 18:29:14 +0200

    [upgrade] Rework LV bundling

--- Superbuild/Projects/apple/lidarview.bundle.cmake
+++ Superbuild/Projects/apple/lidarview.bundle.cmake
@@ -1,16 +1,13 @@
 # Bundling Scripts Stack Entry for $lidarview_appname - Apple Specific
 include(lidarview.bundle.common)
 
-# append non-common lidarview modules to be processed in LidarviewBundle
-list(APPEND lidarview_modules "VelodynePluginPython.so")
-list(APPEND lidarview_modules "libVelodynePluginPythonD.dylib")
-
 # Trigger Apple-specific LidarView Bundling
 include(${LidarViewSuperBuild_CMAKE_DIR}/bundle/apple/LidarviewBundle.cmake)
 
-# Sensor calibration files
-file(GLOB shared_files "${superbuild_install_location}/bin/${lidarview_appname}/Contents/Resources/*.xml")
+# LidarView-Apple Specifics
+
+# Install Sensor calibration files
+file(GLOB shared_files "${share_path}/*.xml")
 install(FILES ${shared_files}
-        DESTINATION "${lidarview_appname}/Contents/Resources"
-        COMPONENT superbuild)
+        DESTINATION "${share_dest}")
 unset(shared_files)
