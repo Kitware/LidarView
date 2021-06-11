d9f58bed663512b1ded940afe854dfc84ec97b02
Arnaud Billon <arnaud.billon@kitware.com>
2021-06-11 11:35:55 +0200

    [Patch] Fix Runtime ZLIB-SYSTEM_Qt5 conflict

--- LVCore/CMake/FinalizeLidarViewProject.cmake
+++ LVCore/CMake/FinalizeLidarViewProject.cmake
@@ -1,7 +1,15 @@
-# reset boost RPATH on OSX
+# Patch reset boost RPATH on OSX
 # this is applied on LidarView app and tests
 # for more information see comments in lidarview.osx-boost-rpath.cmake
 install(SCRIPT "LVCore/CMake/scripts/lidarview.osx-boost-rpath.cmake")
+
+# Patch ZLIB RPATH on Unix/OSX
+# this is applied on LidarView app and tests
+# for more information see comments in lidarview.unix-zlib-link.cmake
+if(ENABLE_pcl AND UNIX AND NOT APPLE)
+  message(WARNING "Forcing zlib.1.2.11 NEEDED using `patchelf` in Lidarplugin to avoid PCL/QT conflicts")
+  install(SCRIPT "LVCore/CMake/scripts/lidarview.unix-zlib-link.cmake")
+endif()
 
 # Output CMakeCacheLog.txt
 install(SCRIPT "LVCore/CMake/OutputCMakeCacheLog.cmake")
