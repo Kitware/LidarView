5198f2c3d54b5af42c2cd572c257e4d3c504f34e
Arnaud Billon <arnaud.billon@kitware.com>
2021-06-22 13:37:36 +0200

    [FinalizeLidarViewProject][WIP] Rework RPATH patches for PV59

--- LVCore/CMake/FinalizeLidarViewProject.cmake
+++ LVCore/CMake/FinalizeLidarViewProject.cmake
@@ -15,15 +15,18 @@ if(NOT paraview_version )
   message(FATAL_ERROR "paraview_version not set")
 endif()
 
+# WIPWIP DISABLED FOR NOW, MOST LIKELY NOT NEEDED ON PV59
+#[[
 # Patch reset boost/flann RPATH on OSX
 # this is applied on LidarView app and tests
 # for more information see comments in lidarview.osx-rpath.cmake
 install(SCRIPT "LVCore/CMake/scripts/lidarview.osx-rpath.cmake")
+]]
 
 # Patch ZLIB RPATH on Unix/OSX
 # this is applied on LidarView app and tests
 # for more information see comments in lidarview.unix-zlib-link.cmake
-if(ENABLE_pcl AND UNIX AND NOT APPLE)
+if(UNIX AND NOT APPLE)
   message(WARNING "Forcing zlib.1.2.11 NEEDED using `patchelf` in Lidarplugin to avoid PCL/QT conflicts")
   install(SCRIPT "LVCore/CMake/scripts/lidarview.unix-zlib-link.cmake")
 endif()
