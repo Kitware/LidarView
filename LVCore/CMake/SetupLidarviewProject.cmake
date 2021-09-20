29772f213a2198e15e21b302b3ebf8fbe4fff25d
Arnaud Billon <arnaud.billon@kitware.com>
2021-09-20 14:33:56 +0200

    [upgrade] use LV_BUILD_PLATFORM

--- LVCore/CMake/SetupLidarviewProject.cmake
+++ LVCore/CMake/SetupLidarviewProject.cmake
@@ -19,7 +19,7 @@ cmake_policy(SET CMP0057 NEW) #if() supports IN_LIST #ParaviewPlugin.cmake
 #This allows incremental build within the ExternalProject dir `lidarview-sb/common-sb/lidarview/build`
 set(SOFTWARE_NAME   ${SOFTWARE_NAME}   CACHE INTERNAL "LidarView-Based app name")
 set(SOFTWARE_VENDOR ${SOFTWARE_VENDOR} CACHE INTERNAL "LidarView-Based app vendor")
-set(LV_BUILD_ARCHITECTURE ${LV_BUILD_ARCHITECTURE} CACHE INTERNAL "Architecture address byte width")
+set(LV_BUILD_PLATFORM ${LV_BUILD_PLATFORM} CACHE INTERNAL "Platform Distribution and Architecture")
 
 #Sanitize checks
 if(NOT SOFTWARE_NAME OR NOT SOFTWARE_VENDOR)
