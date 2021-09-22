fc504d81fb5d230865f8cd85fa0bcb6a3808872b
Arnaud Billon <arnaud.billon@kitware.com>
2021-09-22 10:36:24 +0200

    [upgrade] Enable Optimization for Release build

--- LVCore/CMake/SetCompilationFlags.cmake
+++ LVCore/CMake/SetCompilationFlags.cmake
@@ -34,18 +34,36 @@ endif()
 
 # Warnings
 if (MSVC)
-    # Warning level 3, but disable some noisy warnings
-    add_compile_options(/W3
-                        /wd4244  # 'argument' : conversion from 'type1' to 'type2', possible loss of data
-                        /wd4267  # 'var' : conversion from 'size_t' to 'type', possible loss of data
-                        /wd4800  # 'type1': forcing value to bool 'true' or 'false'
-                        /wd4141  # 'modifier' : used more than once
-                        /wd4251  # 'type' : class 'type1' needs to have dll-interface to be used by clients of class 'type2'
-                        /wd4503  # 'identifier' : decorated name length exceeded, name was truncated
-    )
-
+  # Warning level 3, but disable some noisy warnings
+  add_compile_options(/W3
+                      /wd4244  # 'argument' : conversion from 'type1' to 'type2', possible loss of data
+                      /wd4267  # 'var' : conversion from 'size_t' to 'type', possible loss of data
+                      /wd4800  # 'type1': forcing value to bool 'true' or 'false'
+                      /wd4141  # 'modifier' : used more than once
+                      /wd4251  # 'type' : class 'type1' needs to have dll-interface to be used by clients of class 'type2'
+                      /wd4503  # 'identifier' : decorated name length exceeded, name was truncated
+  )
 else()
-    # Warning level all and extra
-    add_compile_options(-Wall -Wextra -Wsuggest-override)
+  # Warning level all and extra
+  add_compile_options(-Wall -Wextra -Wsuggest-override)
+endif()
 
+# Optimize Build in Release
+string(TOLOWER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_LOWER)
+if (CMAKE_BUILD_TYPE_LOWER STREQUAL "release")
+  # Optimization-Level
+  if(MSVC)
+    add_compile_options(/O2)
+  else()
+    add_compile_options(-O2)
+  endif()
+
+  #IPO
+  include(CheckIPOSupported)
+  check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_ERROR)
+  if(IPO_SUPPORTED)
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+  else()
+    message(WARNING "IPO is not supported: ${IPO_ERROR}")
+  endif()
 endif()
