ccd5fd3cfe4bb117e74c4cc884d5c5493d7e80f0
MelanieCarriere <melanie.carriere@kitware.com>
2021-10-25 14:12:21 +0200

    [doc] Add a simple interpreter example

---
+++ LVCore/Examples/Plugins/InterpreterExample/vtkExamplePacketInterpreter.cxx
@@ -0,0 +1,225 @@
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    vtkExamplePacketInterpreter.cxx
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkExamplePacketInterpreter.h"
+#include "InterpreterHelper.h"
+
+#include "ExampleFormat.h"
+
+#include <boost/filesystem.hpp>
+#include <vtkDoubleArray.h>
+#include <vtkPointData.h>
+#include <vtkPoints.h>
+#include <vtkTransform.h>
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkExamplePacketInterpreter)
+
+//-----------------------------------------------------------------------------
+vtkExamplePacketInterpreter::vtkExamplePacketInterpreter()
+{
+  this->ParserMetaData.SpecificInformation = std::make_shared<ExampleSpecificFrameInformation>();
+
+  this->ResetCurrentFrame();
+}
+
+//-----------------------------------------------------------------------------
+vtkExamplePacketInterpreter::~vtkExamplePacketInterpreter()
+{
+  // delete this->CurrentFrameState;
+}
+
+//-----------------------------------------------------------------------------
+void vtkExamplePacketInterpreter::ProcessPacket(unsigned char const* data, unsigned int dataLength)
+{
+  if (!this->IsLidarPacket(data, dataLength))
+  {
+    return;
+  }
+
+  ExampleSpecificFrameInformation* frameInfo = reinterpret_cast<ExampleSpecificFrameInformation*>(
+    this->ParserMetaData.SpecificInformation.get());
+
+  const LidarPacket* dataPacket = reinterpret_cast<const LidarPacket*>(data);
+
+  // Detection of a new frame
+  // In this example ...
+
+  if (dataPacket->header.GetFrameID() != frameInfo->frameID)
+  {
+    this->SplitFrame();
+    this->LastTimestamp = std::numeric_limits<unsigned int>::max();
+  }
+
+  // Update the transforms here and then call internal transform
+  if (this->SensorTransform)
+  {
+    this->SensorTransform->Update();
+  }
+
+  for (unsigned int i = 0; i < numberOfChannels; i++)
+  {
+    double pos[3] = { 0, 0, 0 };
+
+    // Apply sensor transform
+    if (this->SensorTransform)
+    {
+      this->SensorTransform->InternalTransformPoint(pos, pos);
+    }
+
+    // Test if the point should be displayed
+    if ((this->IgnoreZeroDistances && dataPacket->firings[i].GetDistance() == 0) ||
+      this->shouldBeCroppedOut(pos))
+    {
+      return;
+    }
+
+    this->Points->InsertNextPoint(pos);
+    InsertNextValueIfNotNull(this->PointsX, pos[0]);
+    InsertNextValueIfNotNull(this->PointsY, pos[1]);
+    InsertNextValueIfNotNull(this->PointsZ, pos[2]);
+    InsertNextValueIfNotNull(this->Azimuth, 0);
+    InsertNextValueIfNotNull(this->Intensity, 0);
+    InsertNextValueIfNotNull(this->LaserId, 0);
+    InsertNextValueIfNotNull(this->Timestamp, 0);
+    InsertNextValueIfNotNull(this->Distance, 0);
+  }
+}
+
+//-----------------------------------------------------------------------------
+bool vtkExamplePacketInterpreter::IsLidarPacket(unsigned char const* vtkNotUsed(data),
+  unsigned int dataLength)
+{
+  return dataLength == sizeof(FiringBlock);
+}
+
+//-----------------------------------------------------------------------------
+vtkSmartPointer<vtkPolyData> vtkExamplePacketInterpreter::CreateNewEmptyFrame(vtkIdType nbrOfPoints,
+  vtkIdType prereservedNbrOfPoints)
+{
+  const int defaultPrereservedNbrOfPointsPerFrame = 60000;
+  // prereserve for 50% points more than actually received in previous frame
+  prereservedNbrOfPoints =
+    std::max(static_cast<int>(prereservedNbrOfPoints * 1.5), defaultPrereservedNbrOfPointsPerFrame);
+
+  vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
+
+  // Initialize points
+  vtkNew<vtkPoints> points;
+  points->SetDataTypeToFloat();
+  points->Allocate(prereservedNbrOfPoints);
+  if (nbrOfPoints > 0)
+  {
+    points->SetNumberOfPoints(nbrOfPoints);
+  }
+  points->GetData()->SetName("Points");
+  polyData->SetPoints(points.GetPointer());
+
+  // Initialize data arrays
+  this->Points = points.GetPointer();
+  InitArrayForPolyData(true,
+    this->PointsX,
+    "X",
+    nbrOfPoints,
+    prereservedNbrOfPoints,
+    polyData,
+    this->EnableAdvancedArrays);
+  InitArrayForPolyData(true,
+    this->PointsY,
+    "Y",
+    nbrOfPoints,
+    prereservedNbrOfPoints,
+    polyData,
+    this->EnableAdvancedArrays);
+  InitArrayForPolyData(true,
+    this->PointsZ,
+    "Z",
+    nbrOfPoints,
+    prereservedNbrOfPoints,
+    polyData,
+    this->EnableAdvancedArrays);
+  InitArrayForPolyData(
+    false, this->Intensity, "intensity", nbrOfPoints, prereservedNbrOfPoints, polyData);
+  // InitArrayForPolyData(
+  //   false, this->Channel, "channel", nbrOfPoints, prereservedNbrOfPoints, polyData);
+  InitArrayForPolyData(
+    false, this->Azimuth, "azimuth", nbrOfPoints, prereservedNbrOfPoints, polyData);
+  InitArrayForPolyData(
+    false, this->Distance, "distance", nbrOfPoints, prereservedNbrOfPoints, polyData);
+  InitArrayForPolyData(
+    false, this->Timestamp, "Timestamp", nbrOfPoints, prereservedNbrOfPoints, polyData);
+  InitArrayForPolyData(
+    false, this->VerticalAngle, "vertical_angle", nbrOfPoints, prereservedNbrOfPoints, polyData);
+
+  // Set the default array to display in the application
+  polyData->GetPointData()->SetActiveScalars("intensity");
+
+  return polyData;
+}
+
+//-----------------------------------------------------------------------------
+bool vtkExamplePacketInterpreter::PreProcessPacket(unsigned char const* data,
+  unsigned int vtkNotUsed(dataLength),
+  fpos_t filePosition,
+  double packetNetworkTime,
+  std::vector<FrameInformation>* vtkNotUsed(frameCatalog))
+{
+  const LidarPacket* dataPacket = reinterpret_cast<const LidarPacket*>(data);
+
+  ExampleSpecificFrameInformation* frameInfo = reinterpret_cast<ExampleSpecificFrameInformation*>(
+    this->ParserMetaData.SpecificInformation.get());
+
+  // Detection of a new frame
+  // In this example a frame is detected thanks to a fiel "frame id" in the header of the packet.
+  // This means that in this example there is no new frame detected inside a unique packet.
+  bool isNewFrame = (dataPacket->header.GetFrameID() != frameInfo->frameID);
+  if (isNewFrame)
+  {
+    // A new frame is detected, we flush the previous one
+    this->SplitFrame();
+
+    // We also update the information for the new frame
+    // You need to set the file position to determine the beginning of a frame
+    this->ParserMetaData.FilePosition = filePosition;
+    this->ParserMetaData.FirstPacketDataTime = this->ParserMetaData.FirstPacketNetworkTime =
+      packetNetworkTime;
+  }
+
+  return isNewFrame;
+}
+
+//-----------------------------------------------------------------------------
+std::string vtkExamplePacketInterpreter::GetSensorInformation(bool vtkNotUsed(shortVersion))
+{
+  std::stringstream streamInfo;
+  streamInfo << "Example Sensor";
+
+  return std::string(streamInfo.str());
+}
+
+//-----------------------------------------------------------------------------
+// std::string vtkExamplePacketInterpreter::GetSensorName()
+// {
+//   return "ExampleSensor" + std::to_string(this->GetNumberOfChannels());
+// }
+
+void vtkExamplePacketInterpreter::LoadCalibration(const std::string& filename)
+{
+  if (filename.empty() || boost::filesystem::extension(filename) != ".csv")
+  {
+    this->IsCalibrated = false;
+    return;
+  }
+}
---
+++ LVCore/Examples/Plugins/InterpreterExample/vtkExamplePacketInterpreter.h
@@ -0,0 +1,99 @@
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    vtkExamplePacketInterpreter.h
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkExamplePacketInterpreter_h
+#define vtkExamplePacketInterpreter_h
+
+#include <vtkStringArray.h>
+#include <vtkTypeInt64Array.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkUnsignedIntArray.h>
+#include <vtkUnsignedLongArray.h>
+#include <vtkUnsignedShortArray.h>
+
+#include <memory>
+
+#include "vtkLidarPacketInterpreter.h"
+
+#include "ExamplePacketInterpretersModule.h"
+
+class EXAMPLEPACKETINTERPRETERS_EXPORT vtkExamplePacketInterpreter
+  : public vtkLidarPacketInterpreter
+{
+public:
+  static vtkExamplePacketInterpreter* New();
+  vtkTypeMacro(vtkExamplePacketInterpreter, vtkLidarPacketInterpreter);
+  void PrintSelf(ostream& vtkNotUsed(os), vtkIndent vtkNotUsed(indent)) override{};
+
+  void ProcessPacket(unsigned char const* data, unsigned int dataLength) override;
+
+  bool SplitFrame(bool force = false,
+    FramingMethod_t framingMethodAskingForSplitFrame =
+      FramingMethod_t::INTERPRETER_FRAMING) override;
+
+  bool IsLidarPacket(unsigned char const* data, unsigned int dataLength) override;
+
+  void ResetCurrentFrame() override;
+
+  bool PreProcessPacket(unsigned char const* data,
+    unsigned int dataLength,
+    fpos_t filePosition = fpos_t(),
+    double packetNetworkTime = 0,
+    std::vector<FrameInformation>* frameCatalog = nullptr) override;
+
+  std::string GetSensorInformation(bool shortVersion = false) override;
+
+  // std::string GetSensorName() override;
+
+  void LoadCalibration(const std::string& filename) override;
+
+protected:
+  vtkSmartPointer<vtkPolyData> CreateNewEmptyFrame(vtkIdType nbrOfPoints,
+    vtkIdType prereservedNbrOfPoints = 60000) override;
+
+  vtkSmartPointer<vtkPoints> Points;
+  vtkSmartPointer<vtkDoubleArray> PointsX;
+  vtkSmartPointer<vtkDoubleArray> PointsY;
+  vtkSmartPointer<vtkDoubleArray> PointsZ;
+  vtkSmartPointer<vtkUnsignedCharArray> Intensity;
+  vtkSmartPointer<vtkUnsignedCharArray> LaserId;
+  vtkSmartPointer<vtkUnsignedShortArray> Azimuth;
+  vtkSmartPointer<vtkDoubleArray> Distance;
+  vtkSmartPointer<vtkTypeInt64Array> Timestamp;
+  vtkSmartPointer<vtkDoubleArray> VerticalAngle;
+
+  unsigned int LastTimestamp = 0;
+
+  vtkExamplePacketInterpreter();
+  ~vtkExamplePacketInterpreter();
+
+private:
+  vtkExamplePacketInterpreter(const vtkExamplePacketInterpreter&) = delete;
+  void operator=(const vtkExamplePacketInterpreter&) = delete;
+};
+
+struct ExampleSpecificFrameInformation : public SpecificFrameInformation
+{
+  // Keep the frame id
+  u_int32_t frameID = 0;
+
+  void reset() override { *this = ExampleSpecificFrameInformation(); }
+  std::unique_ptr<SpecificFrameInformation> clone() override
+  {
+    return std::make_unique<ExampleSpecificFrameInformation>(*this);
+  }
+};
+
+#endif // vtkExamplePacketInterpreter_h
