e6c06e7dfa0c1c18220025af1082f877ca0c80a5
Arnaud Billon <arnaud.billon@kitware.com>
2021-11-22 14:24:17 +0100

    [feat] Ship Pandar128 CalibrationFile

--- Superbuild/Projects/unix/lidarview.bundle.cmake
+++ Superbuild/Projects/unix/lidarview.bundle.cmake
@@ -6,8 +6,8 @@ include(${LidarViewSuperBuild_CMAKE_DIR}/bundle/unix/LidarviewBundle.cmake)
 
 # LidarView-Unix Specifics
 
-# Install Sensor calibration files
-file(GLOB shared_files "${share_path}/*.xml")
+# Sensor calibration files
+file(GLOB shared_files "${superbuild_install_location}/share/*.csv")
 install(FILES ${shared_files}
         DESTINATION "${LV_INSTALL_RESOURCE_DIR}")
 unset(shared_files)
