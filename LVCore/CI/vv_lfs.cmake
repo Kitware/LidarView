707553745bc7a79397578f0396f0aa4ff5e15ecf
Arnaud Billon <arnaud.billon@kitware.com>
2022-02-22 13:47:03 +0100

    [ci] Make VelodynePrivate Workaround conditional

---
+++ LVCore/CI/vv_lfs.cmake
@@ -0,0 +1,28 @@
+# Enable Check
+if(NOT CI_VELODYNEPRIVATE_ENABLE)
+  message(STATUS "VelodynePrivate LFS Fetch not requested")
+  return()
+else()
+  message(STATUS "VelodynePrivate LFS Fetch requested")
+endif()
+
+Find_package(Git QUIET)
+if(NOT GIT_EXECUTABLE)
+  message(FATAL_ERROR "error: could not find git")
+endif()
+
+# Set Variables
+SET(VV_PLUGIN_PATH "${CMAKE_CURRENT_LIST_DIR}/../../Plugins/VelodynePlugin")
+
+# Exec Git Command
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} -C ${VV_PLUGIN_PATH} fetch private --prune
+  COMMAND ${GIT_EXECUTABLE} -C ${VV_PLUGIN_PATH} fetch origin --prune
+  RESULT_VARIABLE result
+  OUTPUT_VARIABLE output
+  ERROR_VARIABLE  error
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  ERROR_STRIP_TRAILING_WHITESPACE
+)
+message(STATUS "output: ${output}") #CMake 3.18+ feature
+message(STATUS "error:  ${error}")
---
+++ LVCore/CI/vv_remote.cmake
@@ -0,0 +1,45 @@
+# Enable Check
+if(NOT CI_VELODYNEPRIVATE_ENABLE)
+  message(STATUS "VelodynePrivate Remote Addition not requested")
+  return()
+else()
+  message(STATUS "VelodynePrivate Remote Addition requested")
+endif()
+
+# Sanity Checks
+if(NOT CI_VELODYNEPRIVATE_USER)
+  message(FATAL_ERROR "CI_VELODYNEPRIVATE_USER not set")
+  return()
+endif()
+if(NOT CI_VELODYNEPRIVATE_TOKEN)
+  message(FATAL_ERROR "CI_VELODYNEPRIVATE_TOKEN not set")
+  return()
+endif()
+
+Find_package(Git QUIET)
+if(NOT GIT_EXECUTABLE)
+  message(FATAL_ERROR "error: could not find git")
+endif()
+
+# Set Variables
+SET(VV_REMOTE_URL "https://${CI_VELODYNEPRIVATE_USER}:${CI_VELODYNEPRIVATE_TOKEN}@gitlab.kitware.com/LidarView/velodyneplugin-private.git")
+SET(VV_PLUGIN_PATH "${CMAKE_CURRENT_LIST_DIR}/../../Plugins/VelodynePlugin")
+
+# Exec Git Command
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} -C ${VV_PLUGIN_PATH} remote add private ${VV_REMOTE_URL}
+  COMMAND ${GIT_EXECUTABLE} -C ${VV_PLUGIN_PATH} -vv
+  RESULT_VARIABLE result
+  OUTPUT_VARIABLE output
+  ERROR_VARIABLE  error
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  ERROR_STRIP_TRAILING_WHITESPACE
+)
+message(STATUS "output: ${output}") #CMake 3.18+ feature
+message(STATUS "error:  ${error}")
+if(result)
+  string(FIND output "already exists" find_alreadyexists) #Adding the same remote is okay
+  if(NOT find_alreadyexists)
+      message(FATAL_ERROR "Adding remote failed")
+  endif()
+endif()
