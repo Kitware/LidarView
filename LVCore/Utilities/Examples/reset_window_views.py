18a6459c7256450164225887c3f9954bbdacb976
Timothée Couble <timothee.couble@kitware.com>
2022-11-21 15:32:41 +0100

    [feat] Add Python reset_windows_views example

---
+++ LVCore/Utilities/Examples/reset_window_views.py
@@ -0,0 +1,33 @@
+"""
+Delete all render windows views and recreate a default one  
+"""
+
+from paraview.simple import *
+import lidarview.applogic as lv
+
+# Get sources
+reader = lv.getReader()
+measurementGrid = FindSource('Measurement Grid')
+
+# Get views
+views = GetRenderViews()
+layout = GetLayout()
+
+# Deletes all views
+for view in views:
+    idx = layout.GetViewLocation(view)
+    Delete(view)
+    del view
+    layout.Collapse(idx)
+
+# Create and assign a new one
+renderView = CreateView('RenderView')
+layout.AssignView(0, renderView)
+
+# Show measurement grid and reader
+display = Show(reader[0], renderView)
+ColorBy(display, ('POINTS', 'intensity'))
+Show(measurementGrid, renderView)
+
+# Reset Camera
+lv.resetCameraToForwardView()
