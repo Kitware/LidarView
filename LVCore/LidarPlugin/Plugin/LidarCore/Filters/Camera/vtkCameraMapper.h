9e2643e909278c5073247c842ed92e8ea61bbe1c
Timothée Couble <timothee.couble@kitware.com>
2023-03-09 16:20:01 +0100

    [refact] Add Filters/Camera vtk module

--- LVCore/LidarPlugin/Plugin/LidarCore/Filters/Camera/vtkCameraMapper.h
+++ LVCore/LidarPlugin/Plugin/LidarCore/Filters/Camera/vtkCameraMapper.h
@@ -1,8 +1,6 @@
 //=========================================================================
 //
-// Copyright 2019 Kitware, Inc.
-// Author: Guilbert Pierre (spguilbert@gmail.com)
-// Data: 03-27-2019
+// Copyright 2020 Kitware, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,42 +15,52 @@
 // limitations under the License.
 //=========================================================================
 
-#ifndef VTK_CAMERA_PROJECTOR_H
-#define VTK_CAMERA_PROJECTOR_H
+#ifndef VTK_CAMERA_MAPPER_H
+#define VTK_CAMERA_MAPPER_H
 
 // VTK
-#include <vtkImageAlgorithm.h>
+#include <vtkPolyDataAlgorithm.h>
 #include <vtkSmartPointer.h>
-#include "Common/vtkCustomTransformInterpolator.h"
+#include <vtkImageData.h>
 
 // EIGEN
 #include <Eigen/Dense>
 
 // LOCAL
 #include "CameraModel.h"
+#include "vtkCustomTransformInterpolator.h"
 
-#include "LidarCoreModule.h"
+#include "lvFiltersCameraModule.h"
 
-class LIDARCORE_EXPORT vtkCameraProjector : public vtkImageAlgorithm
+//! Map 3D points to their projection using a camera model. The output contains
+//! texture coordinates that can be used for texture mapping
+
+class LVFILTERSCAMERA_EXPORT vtkCameraMapper : public vtkPolyDataAlgorithm
 {
 public:
-  static vtkCameraProjector *New();
-  vtkTypeMacro(vtkCameraProjector, vtkImageAlgorithm)
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  static vtkCameraMapper *New();
+  vtkTypeMacro(vtkCameraMapper, vtkPolyDataAlgorithm)
 
-  void SetFileName(const std::string &argfilename);
+  void SetCalibrationFileName(const std::string &argfilename);
+  void SetImgSize(double width, double height)
+  {
+    this->ImgSize(0) = width; this->ImgSize(1) = height;
+    this->Modified();
+  };
 
-  vtkSetMacro(ProjectedPointSizeInImage, int);
   vtkSetMacro(UseTrajectoryToCorrectPoints, bool);
+  vtkSetMacro(UseTrajectoryToCompensateCameraMovement, bool);
   vtkSetMacro(PipelineTimeToLidarTime, double);
 
+  void SetCameraMask(const std::string &argfilename);
+  vtkSetMacro(UseCameraMask, bool);
+  vtkSetMacro(InvertCameraMask, bool);
+
 protected:
-  vtkCameraProjector();
+  vtkCameraMapper();
 
   int FillInputPortInformation(int port, vtkInformation *info) override;
-  int FillOutputPortInformation(int port, vtkInformation *info) override;
-  int RequestInformation(vtkInformation* request,
-                         vtkInformationVector** inputVector,
-                         vtkInformationVector* outputVector) override;
   int RequestUpdateExtent(vtkInformation* request,
                           vtkInformationVector** inputVector,
                           vtkInformationVector* outputVector) override;
@@ -61,24 +69,19 @@ protected:
   void Modified() override;
 
 private:
-  vtkCameraProjector(const vtkCameraProjector&) = delete;
-  void operator=(const vtkCameraProjector&) = delete;
+  vtkCameraMapper(const vtkCameraMapper&) = delete;
+  void operator=(const vtkCameraMapper&) = delete;
 
   //! Camera model
   CameraModel Model;
   double CurrentImagePipelineTime = 0.0; // time of the image given to RequestData
 
+  //! Trajectory (TemporalTransforms) of the sensor that produced the point cloud
+  //! or Trajectory followed by the camera (if UseTrajectoryToCompensateCameraMovement is on).
   vtkSmartPointer<vtkCustomTransformInterpolator> Trajectory = nullptr;
 
-
   //! File containing the camera model and parameters
-  std::string Filename;
-
-  //! Name of the color array
-  std::string ColorArrayName = "RGB";
-
-  //! Size of the points in image with projection
-  int ProjectedPointSizeInImage = 1;
+  std::string CalibrationFileName;
 
   //! Should the cache be refreshed before next projection ?
   bool NeedsToUpdateCachedValues = false;
@@ -90,12 +93,35 @@ private:
   //! - undistortion (lidar frame capturing is not instantaneous)
   bool UseTrajectoryToCorrectPoints = true;
 
+  //! Should the trajectory (if available) be used to correct the camera pose?
+  //! the trajectory (poses) will be used to compensate the camera movement.
+  //! This can be used to map the camera with a point cloud that has already been
+  //! transformed using this trajectory.
+  bool UseTrajectoryToCompensateCameraMovement = true;
+
   //! Timeshift to apply to the pipeline time to get the lidar time.
   //! This value could be computed on the current frame, but it much better to
   //! compute an estimator such as the median using all lidar frames
   //! used only if a trajectory is provided and UseTrajectoryToCorrectPoints is
   //! true
   double PipelineTimeToLidarTime = 0.0;
+
+  //! Image size used to create relative coordinates from pixel coordinates in
+  //! case the image input not provided
+  Eigen::Vector2d ImgSize = Eigen::Vector2d(1920., 1080.);
+
+  //! Should we use a mask to invalidate some of the mapped points
+  //! This is typically used in case part of the camera field of view is occluded
+  //! For example a camera seeing egovehicle pixels that don't correspond to
+  //! lidar points
+  bool UseCameraMask = false;
+
+  //! Camera mask Path to the image file used as texture mask (the image must be the same size
+  // as the texture image source)
+  vtkSmartPointer<vtkImageData> CameraMask;
+
+  //! Should we invert mask values?
+  bool InvertCameraMask = false;
 };
 
-#endif // VTK_CAMERA_PROJECTOR_H
+#endif // VTK_CAMERA_MAPPER_H
--- LVCore/LidarPlugin/Plugin/LidarCore/Filters/Camera/vtkCameraProjector.h
+++ LVCore/LidarPlugin/Plugin/LidarCore/Filters/Camera/vtkCameraProjector.h
@@ -1,6 +1,8 @@
 //=========================================================================
 //
-// Copyright 2020 Kitware, Inc.
+// Copyright 2019 Kitware, Inc.
+// Author: Guilbert Pierre (spguilbert@gmail.com)
+// Data: 03-27-2019
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,52 +17,42 @@
 // limitations under the License.
 //=========================================================================
 
-#ifndef VTK_CAMERA_MAPPER_H
-#define VTK_CAMERA_MAPPER_H
+#ifndef VTK_CAMERA_PROJECTOR_H
+#define VTK_CAMERA_PROJECTOR_H
 
 // VTK
-#include <vtkPolyDataAlgorithm.h>
+#include <vtkImageAlgorithm.h>
 #include <vtkSmartPointer.h>
-#include <vtkImageData.h>
-#include "Common/vtkCustomTransformInterpolator.h"
 
 // EIGEN
 #include <Eigen/Dense>
 
 // LOCAL
 #include "CameraModel.h"
+#include "vtkCustomTransformInterpolator.h"
 
-#include "LidarCoreModule.h"
+#include "lvFiltersCameraModule.h"
 
-//! Map 3D points to their projection using a camera model. The output contains
-//! texture coordinates that can be used for texture mapping
-
-class LIDARCORE_EXPORT vtkCameraMapper : public vtkPolyDataAlgorithm
+class LVFILTERSCAMERA_EXPORT vtkCameraProjector : public vtkImageAlgorithm
 {
 public:
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  static vtkCameraMapper *New();
-  vtkTypeMacro(vtkCameraMapper, vtkPolyDataAlgorithm)
+  static vtkCameraProjector *New();
+  vtkTypeMacro(vtkCameraProjector, vtkImageAlgorithm)
 
-  void SetCalibrationFileName(const std::string &argfilename);
-  void SetImgSize(double width, double height)
-  {
-    this->ImgSize(0) = width; this->ImgSize(1) = height;
-    this->Modified();
-  };
+  void SetFileName(const std::string &argfilename);
 
+  vtkSetMacro(ProjectedPointSizeInImage, int);
   vtkSetMacro(UseTrajectoryToCorrectPoints, bool);
-  vtkSetMacro(UseTrajectoryToCompensateCameraMovement, bool);
   vtkSetMacro(PipelineTimeToLidarTime, double);
 
-  void SetCameraMask(const std::string &argfilename);
-  vtkSetMacro(UseCameraMask, bool);
-  vtkSetMacro(InvertCameraMask, bool);
-
 protected:
-  vtkCameraMapper();
+  vtkCameraProjector();
 
   int FillInputPortInformation(int port, vtkInformation *info) override;
+  int FillOutputPortInformation(int port, vtkInformation *info) override;
+  int RequestInformation(vtkInformation* request,
+                         vtkInformationVector** inputVector,
+                         vtkInformationVector* outputVector) override;
   int RequestUpdateExtent(vtkInformation* request,
                           vtkInformationVector** inputVector,
                           vtkInformationVector* outputVector) override;
@@ -69,19 +61,24 @@ protected:
   void Modified() override;
 
 private:
-  vtkCameraMapper(const vtkCameraMapper&) = delete;
-  void operator=(const vtkCameraMapper&) = delete;
+  vtkCameraProjector(const vtkCameraProjector&) = delete;
+  void operator=(const vtkCameraProjector&) = delete;
 
   //! Camera model
   CameraModel Model;
   double CurrentImagePipelineTime = 0.0; // time of the image given to RequestData
 
-  //! Trajectory (TemporalTransforms) of the sensor that produced the point cloud
-  //! or Trajectory followed by the camera (if UseTrajectoryToCompensateCameraMovement is on).
   vtkSmartPointer<vtkCustomTransformInterpolator> Trajectory = nullptr;
 
+
   //! File containing the camera model and parameters
-  std::string CalibrationFileName;
+  std::string Filename;
+
+  //! Name of the color array
+  std::string ColorArrayName = "RGB";
+
+  //! Size of the points in image with projection
+  int ProjectedPointSizeInImage = 1;
 
   //! Should the cache be refreshed before next projection ?
   bool NeedsToUpdateCachedValues = false;
@@ -93,35 +90,12 @@ private:
   //! - undistortion (lidar frame capturing is not instantaneous)
   bool UseTrajectoryToCorrectPoints = true;
 
-  //! Should the trajectory (if available) be used to correct the camera pose?
-  //! the trajectory (poses) will be used to compensate the camera movement.
-  //! This can be used to map the camera with a point cloud that has already been
-  //! transformed using this trajectory.
-  bool UseTrajectoryToCompensateCameraMovement = true;
-
   //! Timeshift to apply to the pipeline time to get the lidar time.
   //! This value could be computed on the current frame, but it much better to
   //! compute an estimator such as the median using all lidar frames
   //! used only if a trajectory is provided and UseTrajectoryToCorrectPoints is
   //! true
   double PipelineTimeToLidarTime = 0.0;
-
-  //! Image size used to create relative coordinates from pixel coordinates in
-  //! case the image input not provided
-  Eigen::Vector2d ImgSize = Eigen::Vector2d(1920., 1080.);
-
-  //! Should we use a mask to invalidate some of the mapped points
-  //! This is typically used in case part of the camera field of view is occluded
-  //! For example a camera seeing egovehicle pixels that don't correspond to
-  //! lidar points
-  bool UseCameraMask = false;
-
-  //! Camera mask Path to the image file used as texture mask (the image must be the same size
-  // as the texture image source)
-  vtkSmartPointer<vtkImageData> CameraMask;
-
-  //! Should we invert mask values?
-  bool InvertCameraMask = false;
 };
 
-#endif // VTK_CAMERA_MAPPER_H
+#endif // VTK_CAMERA_PROJECTOR_H
