8f388ad2884a7983d7555d9922ae968b2422fe4f
Timothée Couble <timothee.couble@kitware.com>
2023-04-26 20:03:59 +0200

    [feat] Add test for CropAndExport python example

--- LVCore/Examples/Python/crop_and_export_example.py
+++ LVCore/Examples/Python/crop_and_export_example.py
@@ -10,17 +10,14 @@ calibfile = 'C:/Demo/VLP-16.xml'
 interpreter = "Velodyne Meta Interpreter"
 
 ################################################################################
-def CropAndSave(filename, timesteps):
+def CropAndSave(reader, filename, timesteps):
     fileDir = os.path.dirname(filename)
     basenameWithoutExtension = os.path.splitext(os.path.basename(filename))[0]
     filenameTemplate = os.path.join(fileDir, basenameWithoutExtension + '_%04d.csv')
     filenameCroppedTemplate = os.path.join(fileDir, basenameWithoutExtension + 'Cropped_%04d.csv')
 
-    lidarSource = smp.FindSource('LidarReader1')
-
     #set up filter for cropping
-    trailingFrame1 = smp.FindSource('TrailingFrame1')
-    threshold = smp.Threshold(registrationName='Threshold1', Input=trailingFrame1)
+    threshold = smp.Threshold(registrationName='Threshold1', Input=reader)
     threshold.Scalars = ['POINTS', 'distance_m']
 
     ### Note : the below would be replaced by threshold.ThresholdRange = [0.0, 10.0] in former Paraview Versions ( such as in VV 5.1 )
@@ -31,7 +28,7 @@ def CropAndSave(filename, timesteps):
     # Loop over the timesteps of interest
     for i in timesteps:
 
-        timestamp = lidarSource.TimestepValues[i]
+        timestamp = reader.TimestepValues[i]
         smp.GetAnimationScene().SetPropertyWithName('AnimationTime',timestamp)
         #note : this could alternatively be done with lv.getPlayerController().onSeekFrame(i)
 
@@ -41,16 +38,15 @@ def CropAndSave(filename, timesteps):
         filenameFull = filenameTemplate % i
         filenameCropped = filenameCroppedTemplate % i
 
-        smp.SaveData(filenameFull, proxy=trailingFrame1, PointDataArrays=['adjustedtime', 'azimuth', 'distance_m', 'intensity', 'laser_id', 'timestamp', 'vertical_angle'])
+        smp.SaveData(filenameFull, proxy=reader, PointDataArrays=['adjustedtime', 'azimuth', 'distance_m', 'intensity', 'laser_id', 'timestamp', 'vertical_angle'])
         smp.SaveData(filenameCropped, proxy=threshold, PointDataArrays=['adjustedtime', 'azimuth', 'distance_m', 'intensity', 'laser_id', 'timestamp', 'vertical_angle'])
 
 ################################################################################
 
+if __name__ == "__main__":
 # open pcap file
-lvsmp.OpenPCAP(inputfile, calibfile, interpreter)
+    reader = lvsmp.OpenPCAP(inputfile, calibfile, interpreter)
 
-# select frames 10 to 20
-timesteps = range(10, 20)
-CropAndSave(outputfile,timesteps)
-
-
+    # select frames 10 to 20
+    timesteps = range(10, 20)
+    CropAndSave(reader, outputfile, timesteps)
