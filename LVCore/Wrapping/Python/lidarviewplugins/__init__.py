80bb3adf2a0604369a264ad01a129775a9e6329b
Timothée Couble <timothee.couble@kitware.com>
2023-04-26 10:33:29 +0200

    [refact] Rename lidarviewpythonplugin to lidarviewplugins

--- LVCore/Wrapping/Python/lidarviewplugins/__init__.py
+++ LVCore/Wrapping/Python/lidarviewplugins/__init__.py
@@ -1,18 +1,18 @@
-""" lidarviewpythonplugin
+""" lidarviewplugins
 This python module contains paraview "programmable filter"-like plugins:
 Subclasses of `VTKPythonAlgorithmBase`.
 In order to use them in a lidarview app, add the following to the python code
-usedvwhen opening the app (such as `applogic.py`)
+used when opening the app (such as `applogic.py`)
 
 ```
 # At the beginning of the file
-import lidarviewpythonplugin
+import lidarviewplugins
 # When loading other plugins
-smp.LoadPlugin(lidarviewpythonplugin.__file__, remote=False, ns=globals())
+smp.LoadPlugin(lidarviewplugins.__file__, remote=False, ns=globals())
 ```
 
 then you can use the capabilities of the plugin as any other paraview plugin:
 - in a python shell with `smp.YourFilterName(...)`
 - they will appear in the menus in the lidarview UI
 """
-from lidarviewpythonplugin.filters import *
+from lidarviewplugins.SectionalView import *
--- LVCore/Wrapping/Python/lidarviewpythonplugin/filters/__init__.py
+++
@@ -1,1 +0,0 @@
-from lidarviewpythonplugin.filters.SectionalViewFilter import *
