e73d4e4f8e2cfad78175eb98346c448b34ac17f8
Timothée Couble <timothee.couble@kitware.com>
2023-05-30 18:01:21 +0200

    [refact] CI use CMAKE_CONFIGURATION to determine config

---
+++ .gitlab/ci/configure_windows_vs2019.cmake
@@ -0,0 +1,13 @@
+include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
+
+set(lvsb_dir)
+if (NOT DEFINED "ENV{LVSB_PATH}")
+  get_filename_component(lvsb_dir "${CMAKE_CURRENT_LIST_DIR}/../../LVSB-DEPS/install/" ABSOLUTE)
+else ()
+  get_filename_component(lvsb_dir "$ENV{LVSB_PATH}/install/" ABSOLUTE)
+endif ()
+list(APPEND lvsb_dir "${lvsb_dir}/Python")
+message(STATUS "CMAKE_PREFIX_PATH setted to ${lvsb_dir}")
+set(CMAKE_PREFIX_PATH "${lvsb_dir}" CACHE PATH "")
+
+set(Boost_USE_STATIC_LIBS OFF CACHE STRING "")
