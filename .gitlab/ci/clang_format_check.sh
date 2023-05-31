1b64fc7e8411bfdbda8b77f2bc6c4d517cd232e9
Timothée Couble <timothee.couble@kitware.com>
2023-05-31 15:12:57 +0200

    [fix] CI Clang format submodule no file error

--- .gitlab/ci/clang_format_check.sh
+++ .gitlab/ci/clang_format_check.sh
@@ -22,11 +22,17 @@ current_SHA=$(git rev-parse HEAD)
 ancestor_SHA=$(git merge-base $current_SHA origin/master)
 modified_files=$(git diff --name-only  $ancestor_SHA $current_SHA *.{h,cxx,txx})
 
-if [[ -z "${modified_files}" ]]; then
+declare -a existing_files
+
+for file in $modified_files; do
+    test -e $file && echo "test $file" && existing_files+=("$file")
+done
+
+if [[ ${#existing_files[@]} -eq 0 ]]; then
     echo "No c++ files detected in changes for clang-format to run against"
 else
-    echo "Performing clang-format check on: $modified_files"
-    clang-format --dry-run -Werror "$modified_files"
+    echo "Performing clang-format check on: ${existing_files[@]}"
+    clang-format --dry-run -Werror ${existing_files[@]}
     exit_code=`echo $?`
 fi
 
