9e49c31a32422f21ff981fc4c5ee99e6c2f19f58
Timothée Couble <timothee.couble@kitware.com>
2023-05-31 11:03:47 +0200

    [fix] CI trigger submodule force to reset on upstream branch

--- .gitlab/ci/checkout_trigger_submodule.sh
+++ .gitlab/ci/checkout_trigger_submodule.sh
@@ -7,6 +7,7 @@ if [[ -z "${TRIGGER_MODULE_PATH}" ]]; then
 else
     git -C $TRIGGER_MODULE_PATH fetch
     git -C $TRIGGER_MODULE_PATH checkout $TRIGGER_MODULE_BRANCH
+    git -C $TRIGGER_MODULE_PATH reset --hard origin/$TRIGGER_MODULE_BRANCH
     git -C $TRIGGER_MODULE_PATH pull
     git -C $TRIGGER_MODULE_PATH submodule sync --recursive
     git -C $TRIGGER_MODULE_PATH submodule update --init --recursive
