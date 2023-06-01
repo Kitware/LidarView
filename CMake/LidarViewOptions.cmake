a2154ae9b7c62661a10de1c3e3031f81e99204c1
Timothée Couble <timothee.couble@kitware.com>
2023-06-01 12:24:27 +0200

    [refact] Add BUILD_DOCUMENTATION option

--- CMake/LidarViewOptions.cmake
+++ CMake/LidarViewOptions.cmake
@@ -4,6 +4,13 @@
 include(CommonLidarViewOptions)
 
 #========================================================================
+# LIDARVIEW BUILD OPTIONS
+#========================================================================
+
+lidarview_deprecated_setting(doc_default BUILD_DOCUMENTATION BUILD_DOC "OFF")
+option(BUILD_DOCUMENTATION "Enable LidarView Documentation" ${doc_default})
+
+#========================================================================
 # OBSOLETE PLUGIN OPTIONS
 #========================================================================
 lidarview_deprecated_setting(velodyne_interpreter
