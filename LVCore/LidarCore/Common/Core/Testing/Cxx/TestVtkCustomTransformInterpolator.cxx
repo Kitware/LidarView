9488d80570326d87c79a7ffbecf5ff683f18b0f0
Julien Chaize <julien.chaize@kitware.com>
2023-06-08 15:02:45 +0200

    [style] Fix license class reference

--- LVCore/LidarCore/Common/Core/Testing/Cxx/TestVtkCustomTransformInterpolator.cxx
+++ LVCore/LidarCore/Common/Core/Testing/Cxx/TestVtkCustomTransformInterpolator.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   LidarView
-  Module:    TestVtkVoxelGridFilter.cxx
+  Module:    TestVtkCustomTransformInterpolator.cxx
 
   Copyright (c) Kitware, Inc.
   All rights reserved.
--- LVCore/LidarCore/Common/Core/Testing/Cxx/TestVtkMergePointsToPolyDataHelper.cxx
+++ LVCore/LidarCore/Common/Core/Testing/Cxx/TestVtkMergePointsToPolyDataHelper.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   LidarView
-  Module:    TestVtkVoxelGridFilter.cxx
+  Module:    TestVtkMergePointsToPolyDataHelper.cxx
 
   Copyright (c) Kitware, Inc.
   All rights reserved.
