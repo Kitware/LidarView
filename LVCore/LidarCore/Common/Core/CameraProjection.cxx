39a2851a38ab3815c3607f2101a4cced5809103e
Julien Chaize <julien.chaize@kitware.com>
2023-07-11 15:53:41 +0200

    [style] Update class headers

--- LVCore/LidarCore/Common/Core/CameraProjection.cxx
+++ LVCore/LidarCore/Common/Core/CameraProjection.cxx
@@ -1,21 +1,17 @@
-//=========================================================================
-//
-// Copyright 2019 Kitware, Inc.
-// Author: Guilbert Pierre (spguilbert@gmail.com)
-// Data: 03-27-2019
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//=========================================================================
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    CameraProjection.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 
 // LOCAL
 #include "CameraProjection.h"
--- LVCore/LidarCore/Common/Core/vtkPipelineTools.h
+++ LVCore/LidarCore/Common/Core/vtkPipelineTools.h
@@ -1,3 +1,18 @@
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    vtkPipelineTools.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
 // Collection of common tools related to pipeline management
 
 #ifndef VTK_PIPELINE_TOOLS_H
