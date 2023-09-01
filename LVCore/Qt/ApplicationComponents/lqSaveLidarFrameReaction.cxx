c4259d319e1cc915d8de512f9c3624335b14ce86
Timothée Couble <timothee.couble@kitware.com>
2023-09-01 10:32:34 +0200

    [refact] Add missing apache license

--- LVCore/Qt/ApplicationComponents/lqSaveLidarFrameReaction.cxx
+++ LVCore/Qt/ApplicationComponents/lqSaveLidarFrameReaction.cxx
@@ -1,3 +1,18 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  lqSaveLidarFrameReaction.cxx
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
 #include "lqSaveLidarFrameReaction.h"
 
 #include <chrono>
--- LVCore/Qt/ApplicationComponents/lqSaveLidarFrameReaction.h
+++ LVCore/Qt/ApplicationComponents/lqSaveLidarFrameReaction.h
@@ -1,3 +1,18 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  lqSaveLidarFrameReaction.h
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
 #ifndef LQSAVELIDARFRAMEREACTION_H
 #define LQSAVELIDARFRAMEREACTION_H
 
