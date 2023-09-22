fbfde646d48e63171661188f4468296286c4ea09
Timothée Couble <timothee.couble@kitware.com>
2023-09-22 14:42:57 +0200

    [refact] Set PCL 1.11 as minimum version

--- LVCore/CMake/FindLidarViewDependencies.cmake
+++ LVCore/CMake/FindLidarViewDependencies.cmake
@@ -105,7 +105,7 @@ find_package(Boost REQUIRED COMPONENTS atomic filesystem program_options system 
 # PCL dependency - optional
 #--------------------------------------
 if (LIDARVIEW_USE_PCL)
-  find_package(PCL REQUIRED COMPONENTS common kdtree features registration io sample_consensus)
+  find_package(PCL REQUIRED 1.11 COMPONENTS common kdtree features registration io sample_consensus)
   # WIP not clean contains a NIP "-Dno-qhull"
   add_definitions(${PCL_DEFINITIONS})
   print_version(PCL)
