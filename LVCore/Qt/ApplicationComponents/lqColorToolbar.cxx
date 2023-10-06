b320e33dba36632f53937152bd76847b54cacb1d
Timothée Couble <timothee.couble@kitware.com>
2023-10-06 11:58:24 +0200

    [style] Clang-format & update license lqColorToolbar

--- LVCore/Qt/ApplicationComponents/lqColorToolbar.cxx
+++ LVCore/Qt/ApplicationComponents/lqColorToolbar.cxx
@@ -1,36 +1,22 @@
 /*=========================================================================
 
-   Program: ParaView
-   Module:    pqColorToolbar.cxx
+  Program: LidarView
+  Module:  lqColorToolbar.cxx
 
-   Copyright (c) 2005,2006 Sandia Corporation, Kitware Inc.
-   All rights reserved.
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
 
-   ParaView is a free software; you can redistribute it and/or modify it
-   under the terms of the ParaView license version 1.2.
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
 
-   See License_v1.2.txt for the full ParaView license.
-   A copy of this license can be obtained by contacting
-   Kitware Inc.
-   28 Corporate Drive
-   Clifton Park, NY 12065
-   USA
+=========================================================================*/
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-========================================================================*/
 #include "lqColorToolbar.h"
 #include "ui_lqColorToolbar.h"
+
+#include <QWidget>
 
 #include <pqActiveObjects.h>
 #include <pqDisplayColorWidget.h>
@@ -48,12 +34,14 @@ void lqColorToolbar::constructor()
   new pqScalarBarVisibilityReaction(ui.actionScalarBarVisibility);
   new pqEditColorMapReaction(ui.actionEditColorMap);
   new pqResetScalarRangeReaction(ui.actionResetRange);
-  new pqResetScalarRangeReaction(ui.actionRescaleCustomRange, true, pqResetScalarRangeReaction::CUSTOM);
+  new pqResetScalarRangeReaction(
+    ui.actionRescaleCustomRange, true, pqResetScalarRangeReaction::CUSTOM);
 
   pqDisplayColorWidget* display_color = new pqDisplayColorWidget(this) << pqSetName("displayColor");
   this->addWidget(display_color);
 
   QObject::connect(&pqActiveObjects::instance(),
-    SIGNAL(representationChanged(pqDataRepresentation*)), display_color,
+    SIGNAL(representationChanged(pqDataRepresentation*)),
+    display_color,
     SLOT(setRepresentation(pqDataRepresentation*)));
 }
