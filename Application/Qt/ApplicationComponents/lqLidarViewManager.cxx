5915c7f5b0d62a1cd26d43349531265e02fd6dec
Timothée Couble <timothee.couble@kitware.com>
2023-10-24 12:09:26 +0200

    [refact] Update lqLidarViewManager license & style

--- Application/Qt/ApplicationComponents/lqLidarViewManager.cxx
+++ Application/Qt/ApplicationComponents/lqLidarViewManager.cxx
@@ -1,34 +1,18 @@
 /*=========================================================================
 
-   Program: LidarView
-   Module:  lqLidarViewManager.cxx
+  Program:   LidarView
+  Module:    lqLidarViewManager.cxx
 
-   Copyright (c) Kitware Inc.
-   All rights reserved.
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
 
-   LidarView is a free software; you can redistribute it and/or modify it
-   under the terms of the LidarView license.
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
 
-   See LICENSE for the full LidarView license.
-   A copy of this license can be obtained by contacting
-   Kitware Inc.
-   28 Corporate Drive
-   Clifton Park, NY 12065
-   USA
+=========================================================================*/
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-========================================================================*/
 #include "lqLidarViewManager.h"
 
 #include <vtkSMSettings.h>
@@ -51,9 +35,6 @@ lqLidarViewManager::lqLidarViewManager(QObject* parent /*=nullptr*/)
   : Superclass(parent)
 {
 }
-
-//-----------------------------------------------------------------------------
-lqLidarViewManager::~lqLidarViewManager() {}
 
 //-----------------------------------------------------------------------------
 void lqLidarViewManager::SetLidarViewDefaultSettings()
