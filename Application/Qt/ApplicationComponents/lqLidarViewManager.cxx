8a2130a4513ab06fc5adb7d7c04deb2182678720
Timothée Couble <timothee.couble@kitware.com>
2023-11-08 16:04:12 +0100

    [fix] Silent "real time" mode deprecation warning

--- Application/Qt/ApplicationComponents/lqLidarViewManager.cxx
+++ Application/Qt/ApplicationComponents/lqLidarViewManager.cxx
@@ -38,6 +38,7 @@
 
 #include <pqApplicationCore.h>
 #include <pqCoreUtilities.h>
+#include <pqSettings.h>
 #include <pqTabbedMultiViewWidget.h>
 
 #include <array>
@@ -396,6 +397,11 @@ void lqLidarViewManager::setLidarViewDefaultSettings()
     ::setSetting(settings, "array_lookup_tables." + name + ".PVLookupTable.IndexedLookup", 1);
     ::setSetting(settings, "array_lookup_tables." + name + ".PVLookupTable.NumberOfTableValues", 3);
   }
+
+  // Remove warning windows "real time" mode deprecated.
+  // Should be remove once LidarView has figured out how to replace it.
+  pqSettings* appSettings = pqApplicationCore::instance()->settings();
+  appSettings->setValue("pqAnimationViewWidget::updatePlayMode", true);
 }
 
 //-----------------------------------------------------------------------------
