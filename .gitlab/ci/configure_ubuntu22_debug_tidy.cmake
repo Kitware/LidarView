c01028b0f2fd02a026154875b40c05f9c852fa7c
Timothée Couble <timothee.couble@kitware.com>
2024-01-19 15:52:08 +0100

    [ci] Enable hesai plugin tidy

--- .gitlab/ci/configure_ubuntu22_debug_tidy.cmake
+++ .gitlab/ci/configure_ubuntu22_debug_tidy.cmake
@@ -1,8 +1,5 @@
 include("${CMAKE_CURRENT_LIST_DIR}/configure_ubuntu22_debug.cmake")
 
-# Disable hesai plugin as external libary got a lot of warnings
-set(PARAVIEW_ENABLE_PLUGIN_HesaiPlugin OFF CACHE BOOL "")
-
 # Treats warnings as errors
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wall -Wextra" CACHE STRING "")
 
