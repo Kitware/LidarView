057ce03c82e8ec7aa1f3242ebd8e40db25b08701
Timothée Couble <timothee.couble@kitware.com>
2024-04-04 17:03:35 +0200

    [refact] Update generate ground truth script

--- LVCore/Utilities/Scripts/GenerateGroundTruthForTestData.py
+++ LVCore/Utilities/Scripts/GenerateGroundTruthForTestData.py
@@ -7,73 +7,50 @@ This file is a helper in the purpose of create new test data
 
 ** How to use this code **
 
-You need to update all the variables that starts by UPDATE
-
-This code has to be copied and pasted in a programmable source in LidarView
-To create a programmable source : "Sources" -> "Programmable source"
-Or "ctrl-space" -> "Programmable filter"
+Add this script to the python shell, and then call generate_ground_truth with adequate arguments
 
 '''
 
 from paraview import simple as sp
+from lidarview import simple as lsmp
 import os.path
 
+"""
+  filename: PCAP filename of the one you want to create test reference data
+  model : LiDAR Model of the pcap, used to search for calibration (e.g VLP-16)
+  vendor: Name of the interpreter vendor (e.g Velodyne)
+  outfolder: Folder where the GroundTruth folder is ALREADY created
+"""
+def generate_ground_truth(filename, model, vendor, outFolder):
+  args = {"IgnoreZeroDistances":True, "IgnoreEmptyFrames":True, "EnableAdvancedArrays":True, "ShowPartialFrames":True }
 
-# Folder where the GroundTruth folder is ALREADY created
-# example of a good path : LV-Source/Plugins/PluginName/Testing/Data/Sensor/GroundTruth/'
-UPDATE_GroundTruthFolder = ''
+  reader = lsmp.OpenPCAP(filename, model, vendor, **args)
+  sp.Show(reader)
+  reader.UpdatePipeline()
+  reader.UpdatePipelineInformation()
 
-# This sensor name is only used in the name of the vtp files
-UPDATE_SensorName = ''
+  # Save all vtp files in the data
+  saveVTPFolder = os.path.join(outFolder, "{}.vtp".format(model))
+  sp.SaveData(saveVTPFolder, proxy=reader, Writetimestepsasfileseries=1)
 
-# filename and calibration of the pcap you want to create test reference data for
-# example of a good path : LV-Source/Plugins/PluginName/Testing/Data/Sensor/file.pcap/'
-UPDATE_pcapFile = ''
-UPDATE_calibrationFileName = ''
-
-# Specific interpreter Name
-# By default, paraview set the first interpreter in alphabetical order
-# which belongs to the proxy group LidarPacketInterpreter
-# If you want
-UPDATE_interpreterName = ''
-
-
-# Initialize the reader with the pcap and calibration files
-reader = sp.LidarReader()
-reader.FileName = UPDATE_pcapFile
-reader.CalibrationFile = UPDATE_calibrationFileName
-if UPDATE_interpreterName != "":
-  reader.Interpreter = UPDATE_interpreterName
-reader.Interpreter.IgnoreZeroDistances = 1
-reader.Interpreter.IgnoreEmptyFrames = 1
-reader.Interpreter.EnableAdvancedArrays = 1
-reader.ShowPartialFrames = 1
-sp.Show(reader)
-reader.UpdatePipeline()
-reader.UpdatePipelineInformation()
-
-# Save all vtp files in the data
-saveVTPFolder = os.path.join(UPDATE_GroundTruthFolder, "{}.vtp".format(UPDATE_SensorName))
-sp.SaveData(saveVTPFolder, proxy=reader, Writetimestepsasfileseries=1)
-
-# Create the frames_list.txt files that contains the list of all vtp file name
-frames_list_filename = os.path.join(UPDATE_GroundTruthFolder, "frames_list.txt")
-with open(frames_list_filename, "a") as f:
-  f.truncate(0)
-  i = 0
-  name_i = UPDATE_SensorName + "_" + str(i) + ".vtp"
-  currentFile = os.path.join(UPDATE_GroundTruthFolder, name_i)
-  while os.path.isfile(currentFile):
-    f.write(name_i + "\n")
-    i = i + 1
-    name_i = UPDATE_SensorName + "_" + str(i) + ".vtp"
-    currentFile = os.path.join(UPDATE_GroundTruthFolder, name_i)
+  # Create the frames_list.txt files that contains the list of all vtp file name
+  frames_list_filename = os.path.join(outFolder, "frames_list.txt")
+  with open(frames_list_filename, "a") as f:
+    f.truncate(0)
+    i = 0
+    name_i = model + "_" + str(i) + ".vtp"
+    currentFile = os.path.join(outFolder, name_i)
+    while os.path.isfile(currentFile):
+      f.write(name_i + "\n")
+      i = i + 1
+      name_i = model + "_" + str(i) + ".vtp"
+      currentFile = os.path.join(outFolder, name_i)
 
 
-# Write the NetworkTime reference in the file
-time = reader.GetClientSideObject().GetNetworkTimeToDataTime()
-with open(UPDATE_GroundTruthFolder + "/../" + UPDATE_SensorName + "-reference-data.xml", "a") as f2:
-  f2.truncate(0)
-  contents = "<referenceData referenceNetworkTimeToDataTime=\"" + str(time) + "\">"
-  f2.write(contents + "\n")
-  f2.write("</referenceData>")
+  # Write the NetworkTime reference in the file
+  time = reader.GetClientSideObject().GetNetworkTimeToDataTime()
+  with open(outFolder + "/../" + model + "-reference-data.xml", "a") as f2:
+    f2.truncate(0)
+    contents = "<referenceData referenceNetworkTimeToDataTime=\"" + str(time) + "\">"
+    f2.write(contents + "\n")
+    f2.write("</referenceData>")
