b4c907773a1742d179e79792a23da706d42aea11
Timothée Couble <timothee.couble@kitware.com>
2024-06-21 13:16:17 +0200

    [cmake] Update required ParaView version

--- LVCore/CMake/FindLidarViewDependencies.cmake
+++ LVCore/CMake/FindLidarViewDependencies.cmake
@@ -21,7 +21,7 @@ message(STATUS "Using Python ${lidarview_python_version}")
 #--------------------------------------
 # ParaView dependency - required
 #--------------------------------------
-find_package(ParaView 5.11 REQUIRED)
+find_package(ParaView 5.12 REQUIRED)
 set(paraview_version "${ParaView_VERSION_MAJOR}.${ParaView_VERSION_MINOR}")
 if (NOT PARAVIEW_USE_QT)
   message(FATAL_ERROR "PARAVIEW_USE_QT is OFF, Paraview must be built with Qt")
