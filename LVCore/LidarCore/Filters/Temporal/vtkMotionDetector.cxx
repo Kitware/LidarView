c2fbee3a82e55c740021f08dee023f2fe5951f7d
julia.sanchez <julia.sanchez@kitware.com>
2024-07-15 11:35:01 +0200

    [refact] Add ComputeWeightedProba function for clarity

--- LVCore/LidarCore/Filters/Temporal/vtkMotionDetector.cxx
+++ LVCore/LidarCore/Filters/Temporal/vtkMotionDetector.cxx
@@ -129,10 +129,17 @@ public:
     // Weight of the distribution
     double Weight = 1.0;
 
-    // Get gaussian value of point x
+    // Get normal proba of point x
     double operator()(double x) const
     {
       return 1.0 / (this->Sigma * sqrt(2 * vtkMath::Pi())) *
+        exp(-std::pow(x - this->Mean, 2) / (2.0 * std::pow(this->Sigma, 2)));
+    }
+
+    // Get weighted proba of point x
+    double ComputeWeightedProba(double x) const
+    {
+      return this->Weight / (this->Sigma * sqrt(2 * vtkMath::Pi())) *
         exp(-std::pow(x - this->Mean, 2) / (2.0 * std::pow(this->Sigma, 2)));
     }
 
@@ -218,7 +225,7 @@ public:
       this->ItClosest = this->Gaussians.begin();
       for (auto it = this->Gaussians.begin(); it != this->Gaussians.end(); ++it)
       {
-        probas.emplace_back(it->Weight * (*it)(x));
+        probas.emplace_back(it->ComputeWeightedProba(x));
         double dist = std::fabs(x - it->Mean);
         if (dist < minDistance)
         {
