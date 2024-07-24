fbedb1181ceb787d3227356c647ee4826193307a
Timothée Couble <timothee.couble@kitware.com>
2024-07-24 15:19:55 +0200

    [refact] Update license header for vtkStream

--- LVCore/LidarCore/IO/Network/vtkStream.cxx
+++ LVCore/LidarCore/IO/Network/vtkStream.cxx
@@ -1,19 +1,18 @@
-//=========================================================================
-//
-// Copyright 2018 Kitware, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//=========================================================================
+/*=========================================================================
+
+  Program: LidarView
+  Module:  vtkStream.cxx
+
+  Copyright 2013 Velodyne Acoustics, Inc.
+  Copyright 2018 (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 
 #include "vtkStream.h"
 
--- LVCore/LidarCore/IO/Network/vtkStream.h
+++ LVCore/LidarCore/IO/Network/vtkStream.h
@@ -1,20 +1,21 @@
-// Copyright 2013 Velodyne Acoustics, Inc.
-// Copyright 2018 Kitware SAS.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*=========================================================================
 
-#ifndef VTKSTREAM_H
-#define VTKSTREAM_H
+  Program: LidarView
+  Module:  vtkStream.h
+
+  Copyright 2013 Velodyne Acoustics, Inc.
+  Copyright 2018 (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkStream_h
+#define vtkStream_h
 
 #include <memory>
 #include <mutex>
@@ -181,4 +182,4 @@ private:
 #undef vtkDataObjectAlgorithm
 #endif
 
-#endif // VTKSTREAM_H
+#endif // vtkStream_h
