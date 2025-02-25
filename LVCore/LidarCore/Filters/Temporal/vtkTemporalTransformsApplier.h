c90e5a659c2af55a84aa3ebcb0714433c6f20a75
Timothée Couble <timothee.couble@kitware.com>
2025-02-25 10:00:38 +0100

    [refact] Clang-format & update license header

--- LVCore/LidarCore/Filters/Temporal/vtkTemporalTransformsApplier.h
+++ LVCore/LidarCore/Filters/Temporal/vtkTemporalTransformsApplier.h
@@ -1,19 +1,20 @@
-// Copyright 2019 Kitware SAS.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*=========================================================================
 
-#ifndef TemporalTransformsAPPLIER_H
-#define TemporalTransformsAPPLIER_H
+  Program:   LidarView
+  Module:    vtkTemporalTransformApplier.h
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkTemporalTransformApplier_h
+#define vtkTemporalTransformApplier_h
 
 #include <vtkNew.h>
 #include <vtkPolyDataAlgorithm.h>
@@ -32,11 +33,17 @@ class LVFILTERSTEMPORAL_EXPORT vtkTemporalTransformsApplier : public vtkPolyData
 {
 public:
   static vtkTemporalTransformsApplier* New();
-  vtkTypeMacro(vtkTemporalTransformsApplier,vtkPolyDataAlgorithm)
+  vtkTypeMacro(vtkTemporalTransformsApplier, vtkPolyDataAlgorithm);
+
+  /**
+   * Override GetMTime() because we depend on the TransformInterpolator
+   * which may be modified outside of this class.
+   */
+  vtkMTimeType GetMTime() override;
 
   //@{
   /**
-   * @copydoc vtkTemporalTransformsApplier::InterpolationType
+   * Interpolator used to get the right transform
    */
   int GetInterpolationType() { return this->Interpolator->GetInterpolationType(); }
   void SetInterpolationType(int value) { this->Interpolator->SetInterpolationType(value); };
@@ -44,48 +51,41 @@ public:
 
   //@{
   /**
-   * @copydoc vtkTemporalTransformsApplier::InterpolateEachPoint
+   * Indicate if a different transform should be apply to each point,
+   * or if the same transform should be apply to the whole point cloud.
+   * In the first case you must specify the array from the pointcloud containing the
+   * timestamp with 'SetInputArrayToProcess'
    */
-  vtkGetMacro(InterpolateEachPoint, bool)
-  vtkSetMacro(InterpolateEachPoint, bool)
+  vtkGetMacro(InterpolateEachPoint, bool);
+  vtkSetMacro(InterpolateEachPoint, bool);
   //@}
 
+  //@{
   /**
-   * @brief Override GetMTime() because we depend on the TransformInterpolator
-   * which may be modified outside of this class.
+   * Convert time in second default is for data in microsecond
    */
-  vtkMTimeType GetMTime() override;
-
-  vtkGetMacro(ConversionFactorToSecond, double)
-  vtkSetMacro(ConversionFactorToSecond, double)
+  vtkGetMacro(ConversionFactorToSecond, double);
+  vtkSetMacro(ConversionFactorToSecond, double);
+  //@}
 
 protected:
   vtkTemporalTransformsApplier();
 
   int RequestInformation(vtkInformation* request,
-                  vtkInformationVector** inputVector,
-                  vtkInformationVector* outputVector) override;
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
 
   int RequestData(vtkInformation* request,
-                  vtkInformationVector** inputVector,
-                  vtkInformationVector* outputVector) override;
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
 
 private:
-    //! Indicate if a different transform should be apply to each point,
-  //! or if the same transform should be apply to the whole point cloud.
-  //! In the first case you must specify the array from the pointcloud containing the
-  //! timestamp with 'SetInputArrayToProcess'
   bool InterpolateEachPoint;
-
-  //! Interpolator used to get the right transform
   vtkSmartPointer<vtkCustomTransformInterpolator> Interpolator;
-
-  //! double to convert time in second,
-  //! default is for data in microsecond
   double ConversionFactorToSecond = 1e-6;
 
   vtkTemporalTransformsApplier(const vtkTemporalTransformsApplier&) /*= delete*/;
-  void operator =(const vtkTemporalTransformsApplier&) /*= delete*/;
+  void operator=(const vtkTemporalTransformsApplier&) /*= delete*/;
 };
 
-#endif // TemporalTransformsAPPLIER_H
+#endif // vtkTemporalTransformApplier_h
