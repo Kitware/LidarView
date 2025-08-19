6de99fc71c989dacc050d7c37332803fd3a4fbe0
Timothée Couble <timothee.couble@kitware.com>
2025-08-19 10:53:27 +0200

    [refact] Update licenses

--- LVCore/LidarCore/IO/Core/vtkTemporalTransformsWriter.cxx
+++ LVCore/LidarCore/IO/Core/vtkTemporalTransformsWriter.cxx
@@ -1,3 +1,18 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  vtkTemporalTransformsWriter.cxx
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
 #include "vtkTemporalTransformsWriter.h"
 
 #include <iostream>
