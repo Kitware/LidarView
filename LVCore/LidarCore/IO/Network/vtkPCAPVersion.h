e12945948d647b54ccf5ac1d60d4e96c0a471611
Timothée Couble <timothee.couble@kitware.com>
2025-09-10 17:50:58 +0200

    [feat] Add a method to get underlying pcap library used

---
+++ LVCore/LidarCore/IO/Network/vtkPCAPVersion.h
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  vtkPCAPVersion.h
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkPCAPVersion_h
+#define vtkPCAPVersion_h
+
+/**
+ * Utility functions to get the underlying pcap library version.
+ */
+namespace vtkPCAPVersion
+{
+const char* GetPcapLibraryVersion();
+};
+
+#endif
