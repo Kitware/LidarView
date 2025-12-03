2e48fb91d8b6c400b629630ee2e1545725969e72
chenjie lu <chenjie.lu@kitware.com>
2025-12-03 10:37:18 +0100

    [feat] Add image-to-point-cloud color projection filter

---
+++ LVCore/LidarCore/Filters/Visualization/vtkColorPointNeighborImage.cxx
@@ -0,0 +1,150 @@
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    vtkColorPointNeighborImage.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkColorPointNeighborImage.h"
+
+#include <vtkDataArray.h>
+#include <vtkDataSet.h>
+#include <vtkInformation.h>
+#include <vtkInformationVector.h>
+#include <vtkNew.h>
+#include <vtkObjectFactory.h>
+#include <vtkPointData.h>
+#include <vtkPointLocator.h>
+#include <vtkPoints.h>
+#include <vtkPolyData.h>
+#include <vtkStaticPointLocator.h>
+#include <vtkUnsignedCharArray.h>
+
+#include <algorithm>
+#include <cmath>
+
+//-----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkColorPointNeighborImage);
+namespace
+{
+constexpr int POINTS_INPUT_PORT = 0;
+constexpr int IMAGE_INPUT_PORT = 1;
+}
+
+//-----------------------------------------------------------------------------
+vtkColorPointNeighborImage::vtkColorPointNeighborImage()
+{
+  this->SetNumberOfInputPorts(2);
+  this->SetNumberOfOutputPorts(1);
+}
+
+//-----------------------------------------------------------------------------
+vtkColorPointNeighborImage::~vtkColorPointNeighborImage() = default;
+
+//-----------------------------------------------------------------------------
+int vtkColorPointNeighborImage::FillInputPortInformation(int port, vtkInformation* info)
+{
+  if (port == POINTS_INPUT_PORT)
+  {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+    return 1;
+  }
+
+  if (port == IMAGE_INPUT_PORT)
+  {
+    info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
+    return 1;
+  }
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+int vtkColorPointNeighborImage::RequestData(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** inputVector,
+  vtkInformationVector* outputVector)
+{
+  vtkInformation* imgInfo = inputVector[IMAGE_INPUT_PORT]->GetInformationObject(0);
+  vtkDataSet* sgrid = vtkDataSet::GetData(imgInfo);
+  vtkPolyData* inputPoly =
+    vtkPolyData::GetData(inputVector[POINTS_INPUT_PORT]->GetInformationObject(0));
+  vtkPolyData* outputPoly = vtkPolyData::GetData(outputVector->GetInformationObject(0));
+
+  if (!sgrid || !inputPoly)
+  {
+    vtkErrorMacro("Missing input: need vtkPolyData at port 0 and vtkDataSet at port 1");
+    return 0;
+  }
+
+  outputPoly->ShallowCopy(inputPoly);
+
+  vtkPoints* points = inputPoly->GetPoints();
+  if (!points || points->GetNumberOfPoints() == 0)
+  {
+    return 1;
+  }
+
+  // Prepare output color array
+  vtkNew<vtkUnsignedCharArray> colors;
+  colors->SetName("RGB");
+  colors->SetNumberOfComponents(3);
+  colors->SetNumberOfTuples(points->GetNumberOfPoints());
+
+  // Build a locator for nearest neighbor color transfer
+  vtkNew<vtkStaticPointLocator> locator;
+  locator->SetDataSet(sgrid);
+  locator->BuildLocator();
+
+  vtkDataArray* colorArray = nullptr;
+  const char* colorName = this->GetColorArrayName();
+  if (colorName && *colorName)
+  {
+    colorArray = sgrid->GetPointData()->GetArray(colorName);
+  }
+
+  if (!colorArray)
+  {
+    colorArray = sgrid->GetPointData()->GetScalars();
+  }
+
+  double p[3];
+  for (vtkIdType pid = 0; pid < points->GetNumberOfPoints(); ++pid)
+  {
+    points->GetPoint(pid, p);
+    vtkIdType gid = locator->FindClosestPoint(p);
+    unsigned char rgb[3] = { 255, 255, 255 };
+    if (gid >= 0 && colorArray)
+    {
+      if (auto uc = vtkUnsignedCharArray::SafeDownCast(colorArray))
+      {
+        unsigned char tmp[4] = { 0, 0, 0, 0 };
+        uc->GetTypedTuple(gid, tmp);
+        rgb[0] = tmp[0];
+        rgb[1] = uc->GetNumberOfComponents() > 1 ? tmp[1] : tmp[0];
+        rgb[2] = uc->GetNumberOfComponents() > 2 ? tmp[2] : tmp[0];
+      }
+      else if (auto da = vtkDataArray::SafeDownCast(colorArray))
+      {
+        double r = da->GetComponent(gid, 0);
+        double g = da->GetNumberOfComponents() > 1 ? da->GetComponent(gid, 1) : r;
+        double b = da->GetNumberOfComponents() > 2 ? da->GetComponent(gid, 2) : r;
+        rgb[0] = static_cast<unsigned char>(std::lround(std::max(0.0, std::min(255.0, r))));
+        rgb[1] = static_cast<unsigned char>(std::lround(std::max(0.0, std::min(255.0, g))));
+        rgb[2] = static_cast<unsigned char>(std::lround(std::max(0.0, std::min(255.0, b))));
+      }
+    }
+    colors->SetTypedTuple(pid, rgb);
+  }
+
+  outputPoly->GetPointData()->SetScalars(colors);
+
+  return 1;
+}
---
+++ LVCore/LidarCore/Filters/Visualization/vtkColorPointNeighborImage.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+
+  Program:   LidarView
+  Module:    vtkColorPointNeighborImage.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See LICENSE or http://www.apache.org/licenses/LICENSE-2.0 for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkColorPointNeighborImage_h
+#define vtkColorPointNeighborImage_h
+
+#include <string>
+#include <vtkPolyDataAlgorithm.h>
+
+#include "lvFiltersVisualizationModule.h"
+
+class vtkInformation;
+class vtkInformationVector;
+
+/**
+ * The vtkColorPointNeighborImage class projects color information from an
+ * image onto a point cloud using nearest-neighbor lookup.
+ *
+ * This filter takes an image represented as a vtkDataSet and a point
+ * cloud as vtkPolyData. For each point in the cloud, it finds the closest point
+ * in the image grid and transfers its color (RGB or scalar-based) to the point
+ * cloud.
+ *
+ * Compared with classic camera-based colorization (camera image + lidar points +
+ * extrinsic/intrinsic calibration), this filter does not perform camera
+ * projection. It assumes the input image dataset and input point cloud are
+ * already spatially aligned in the same coordinate frame.
+ */
+class LVFILTERSVISUALIZATION_EXPORT vtkColorPointNeighborImage : public vtkPolyDataAlgorithm
+{
+public:
+  static vtkColorPointNeighborImage* New();
+  vtkTypeMacro(vtkColorPointNeighborImage, vtkPolyDataAlgorithm);
+
+  /**
+   * Name of the point-data array on the input image dataset used for colors.
+   */
+  vtkSetStdStringFromCharMacro(ColorArrayName);
+  vtkGetCharFromStdStringMacro(ColorArrayName);
+
+protected:
+  vtkColorPointNeighborImage();
+  ~vtkColorPointNeighborImage() override;
+
+  int FillInputPortInformation(int port, vtkInformation* info) override;
+  int RequestData(vtkInformation* request,
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+
+private:
+  vtkColorPointNeighborImage(const vtkColorPointNeighborImage&) = delete;
+  void operator=(const vtkColorPointNeighborImage&) = delete;
+
+  std::string ColorArrayName;
+};
+
+#endif
