8d020e4de5dfb8611fee9beb44a4a77dc57f0746
tong.fu <tong.fu@kitware.com>
2025-12-18 11:47:20 +0100

    [feat] Add cluster info display filter

---
+++ LVCore/LidarCore/Filters/General/vtkClusterText.cxx
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  vtkClusterText.cxx
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkClusterText.h"
+
+#include <vtkDataArray.h>
+#include <vtkFieldData.h>
+#include <vtkInformation.h>
+#include <vtkIntArray.h>
+#include <vtkMultiBlockDataSet.h>
+#include <vtkStringArray.h>
+#include <vtkTable.h>
+#include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkVectorText.h>
+
+#include <array>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkClusterText);
+
+//----------------------------------------------------------------------------
+int vtkClusterText::RequestData(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** inputVector,
+  vtkInformationVector* outputVector)
+{
+  // Get IO
+  auto input = vtkMultiBlockDataSet::GetData(inputVector[0]);
+  auto output = vtkMultiBlockDataSet::GetData(outputVector, 0);
+
+  unsigned int numBlocks = input->GetNumberOfBlocks();
+  output->SetNumberOfBlocks(numBlocks);
+  for (unsigned int blockId = 0; blockId < numBlocks; ++blockId)
+  {
+    // Get block
+    auto block = vtkDataObject::SafeDownCast(input->GetBlock(blockId));
+    if (!block || !block->GetFieldData())
+    {
+      continue;
+    }
+    // Get field data
+    vtkFieldData* fd = block->GetFieldData();
+    if (!fd || !this->FieldDataArrayName)
+    {
+      continue;
+    }
+    // Get cluster information from field data
+    vtkDataArray* centerArr = vtkDataArray::SafeDownCast(fd->GetArray("Center"));
+    vtkDataArray* heightArr = vtkDataArray::SafeDownCast(fd->GetArray("Height"));
+    vtkIntArray* clusterIdArr = vtkIntArray::SafeDownCast(fd->GetArray("ClusterId"));
+    vtkAbstractArray* valueArray = fd->GetAbstractArray(this->FieldDataArrayName);
+    if (!centerArr || !heightArr || !valueArray || !clusterIdArr)
+    {
+      vtkWarningMacro(<< "Missing required arrays to display cluster information");
+      continue;
+    }
+    double center[3];
+    centerArr->GetTuple(0, center);
+    double height = heightArr->GetTuple1(0);
+
+    // Build text
+    std::ostringstream txt;
+    txt << this->FieldDataArrayName << ": " << std::fixed << std::setprecision(2);
+    if (valueArray->GetNumberOfComponents() == 1)
+    {
+      txt << valueArray->GetVariantValue(0);
+    }
+    else
+    {
+      txt << "(";
+      for (int c = 0; c < valueArray->GetNumberOfComponents(); ++c)
+      {
+        if (c > 0)
+        {
+          txt << ", ";
+        }
+        txt << valueArray->GetVariantValue(c);
+      }
+      txt << ")";
+    }
+    vtkNew<vtkVectorText> text3D;
+    text3D->SetText(txt.str().c_str());
+    text3D->Update();
+
+    vtkNew<vtkTransform> transform;
+    transform->Translate(center[0], center[1], center[2] + height * 0.6);
+    transform->RotateX(90);
+    transform->RotateY(-90);
+    transform->Scale(0.5, 0.5, 0.5);
+
+    vtkNew<vtkTransformPolyDataFilter> tfText;
+    tfText->SetInputConnection(text3D->GetOutputPort());
+    tfText->SetTransform(transform);
+    tfText->Update();
+
+    vtkSmartPointer<vtkPolyData> labelPoly = tfText->GetOutput();
+    labelPoly->GetFieldData()->ShallowCopy(fd);
+    output->SetBlock(blockId, labelPoly);
+    std::string labelblockName("Cluster-" + std::to_string(clusterIdArr->GetValue(0)));
+    output->GetMetaData(blockId)->Set(vtkCompositeDataSet::NAME(), labelblockName.c_str());
+  }
+  return 1;
+}
---
+++ LVCore/LidarCore/Filters/General/vtkClusterText.h
@@ -0,0 +1,57 @@
+/*=========================================================================
+
+  Program: LidarView
+  Module:  vtkClusterText.h
+
+  Copyright (c) Kitware Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef vtkClusterText_h
+#define vtkClusterText_h
+
+// vtk includes
+#include <vtkMultiBlockDataSetAlgorithm.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataAlgorithm.h>
+
+#include "lvFiltersGeneralModule.h"
+
+/**
+ * @class vtkClusterText
+ * @brief Show a cluster information around
+ */
+class LVFILTERSGENERAL_EXPORT vtkClusterText : public vtkMultiBlockDataSetAlgorithm
+{
+public:
+  static vtkClusterText* New();
+  vtkTypeMacro(vtkClusterText, vtkMultiBlockDataSetAlgorithm);
+
+  // Setter/getter
+  // Array name of the cluster information to display
+  vtkSetStringMacro(FieldDataArrayName);
+  vtkGetStringMacro(FieldDataArrayName);
+
+protected:
+  vtkClusterText() = default;
+  ~vtkClusterText() = default;
+
+  int RequestData(vtkInformation* request,
+    vtkInformationVector** inputVector,
+    vtkInformationVector* outputVector) override;
+
+private:
+  vtkClusterText(const vtkClusterText&) = delete;
+  void operator=(const vtkClusterText&) = delete;
+
+  // Array name of the cluster information to display
+  char* FieldDataArrayName = nullptr;
+};
+
+#endif // vtkClusterText_h
