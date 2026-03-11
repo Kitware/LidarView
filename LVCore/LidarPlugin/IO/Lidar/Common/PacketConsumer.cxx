Kitware/VeloView

Fix crash due to packet queue growing limitless
This crash was due to the application running out of memory.
This crash could happen when stresstesting with PacketFileSender,
and possibly when running on slow computers.

Note that the packet queue is not currently limited in the
PacketFileWriter class, because dropping packets there has
more consequences.

LidarPlugin/IO/Lidar/Common/PacketConsumer.cxx
@@ -87,4 +87,16 @@ void PacketConsumer::Stop()
 void PacketConsumer::Enqueue(NetworkPacket* packet)
 {
   this->Packets->enqueue(packet);
+
+  // In order to prevent memory usage to grow unbounded, we limit the growth of
+  // the packet cache. Above an arbitrary limit it seems safe to assume that
+  // the lateness of the consuming (decoding) thread has zero chance of being
+  // catched up and that packets can be dropped.
+  // To test: look at memory usage while running PacketFileSender --speed 100
+  if (this->Packets->size() > this->PacketCacheSize)
+  {
+      NetworkPacket* packet = nullptr;
+      this->Packets->dequeue(packet);
+      delete packet;
+  }
 }

Kitware/LidarView

LVCore/LidarPlugin/IO/Lidar/Common/PacketConsumer.cxx
@@ -84,4 +84,16 @@ void PacketConsumer::Stop()
 void PacketConsumer::Enqueue(NetworkPacket* packet)
 {
   this->Packets->enqueue(packet);
+
+  // In order to prevent memory usage to grow unbounded, we limit the growth of
+  // the packet cache. Above an arbitrary limit it seems safe to assume that
+  // the lateness of the consuming (decoding) thread has zero chance of being
+  // catched up and that packets can be dropped.
+  // To test: look at memory usage while running PacketFileSender --speed 100
+  if (this->Packets->size() > this->PacketCacheSize)
+  {
+      NetworkPacket* packetToDiscard = nullptr;
+      this->Packets->dequeue(packetToDiscard);
+      delete packetToDiscard;
+  }
 }